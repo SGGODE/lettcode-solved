523538e1a48514b961a55f069f2093cd99634321
sanket gode <88069272+SGGODE@users.noreply.github.com>
2022-08-29 18:24:32 +0530

    Time: 52 ms (58.81%), Space: 13.3 MB (27.33%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,33 +1,31 @@
 class Solution {
 private:
-  void dfs(int row,int col,vector<vector<char>>& grid, vector<vector<int>>&vis){
-      vis[row][col]=1; 
-      int n=grid.size();
-      int m=grid[0].size();
-       int dx[4] = {-1, 0, 1, 0};
-            int dy[4] = {0, 1, 0, -1};
-            for(int i=0; i<4; i++){
-             int nr=row+dx[i];
-             int nc=col+dy[i];
-              if(nr>=0 && nc >=0 && nr<n && nc <m && grid[nr][nc]=='1' && !vis[nr][nc]){
-                  dfs(nr,nc,grid,vis); 
-              }
-      }
-  }
+    void dfs(vector<vector<char>>& grid,vector<vector<int>>&vis,int src,int des,int n,int m){
+        vis[src][des]=1;
+        int dx[4]={-1,0,+1,0};
+        int dy[4]={0,+1,0,-1};
+        for(int i=0;i<4;i++){
+           int nr=src+dx[i];
+           int ny=des+dy[i];
+           if(nr >=0 && ny >=0 && nr < n && ny < m
+             && !vis[nr][ny] && grid[nr][ny]=='1')
+               dfs(grid,vis,nr,ny,n,m);
+        }
+    }
 public:
     int numIslands(vector<vector<char>>& grid) {
-      int n=grid.size();
-      int m=grid[0].size();
-      int cnt=0;
-      vector<vector<int>> vis(n,vector<int>(m,0));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' and !vis[i][j]){
-                    cnt++;
-                    dfs(i,j,grid,vis);
-                }
-            }
+     int n=grid.size();
+     int m=grid[0].size();
+     vector<vector<int>> vis(n,vector<int>(m,0));
+     int cnt=0;
+     for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+          if(!vis[i][j]&&grid[i][j]=='1'){
+              cnt++;
+              dfs(grid,vis,i,j,n,m);
+          }  
         }
-        return cnt;
+     }
+      return cnt;
     }
 };
