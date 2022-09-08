e12b41b7fc7ddf6db4a968e34d4e1c314e8b6779
sanket gode <88069272+SGGODE@users.noreply.github.com>
2022-09-08 09:35:01 +0530

    Time: 4 ms (67.49%), Space: 7.9 MB (13.32%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -15,11 +15,33 @@ private:
     int right=get(dp,grid,n,m-1);
     return dp[n][m]=left+right;
 }
+int fro(vector<vector<int>>&dp,vector<vector<int>>&grid,int n,int m){
+    for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+             if(i>=0 && j>=0 && grid[i][j]==1){
+                    dp[i][j]=0;
+                    continue;
+                }
+             if(i==0 && j==0){
+                 dp[i][j]=1;
+                 continue;
+             }
+             int left=0,right=0;
+             if(i>0)
+                left=dp[i-1][j];
+             if(j>0)
+                right=dp[i][j-1];
+              dp[i][j]=left+right;
+            }
+         }
+        return dp[n-1][m-1];
+}
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n=obstacleGrid.size();
         int m=obstacleGrid[0].size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return get(dp,obstacleGrid,n-1,m-1);
+        return fro(dp,obstacleGrid,n,m);
+        //return get(dp,obstacleGrid,n-1,m-1);
     }
 };
