6d445c23409ea48a084198a6fd788750fe675453
sanket gode <88069272+SGGODE@users.noreply.github.com>
2022-09-28 05:25:19 +0530

    Time: 4 ms (43.06%), Space: 7.1 MB (33.31%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-       for(int i=0;i<matrix.size();i++){
-           for(int j=0;j<i;j++){
-               swap(matrix[i][j],matrix[j][i]);
-           }
-       }
-        int n=matrix.size();
-        for(int i=0;i<matrix.size();i++){
-           for(int j=0;j<(n/2);j++){
-               swap(matrix[i][j],matrix[i][n-1-j]);
-           }
-       }
+      int n=matrix.size();
+    int m=matrix[0].size();
+      for(int i=0;i<n;i++){
+          for(int j=0;j<i;j++){
+              swap(matrix[i][j],matrix[j][i]);
+          }
+      }
+      for(int i=0;i<n;i++){
+          reverse(matrix[i].begin(),matrix[i].end());
+      }
     }
 };
