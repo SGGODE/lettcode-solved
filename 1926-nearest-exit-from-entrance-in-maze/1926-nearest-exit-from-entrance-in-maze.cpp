3382f907b34a58064e344cbba226ab50d26ee6e1
sanket gode <88069272+SGGODE@users.noreply.github.com>
2022-11-21 10:05:26 +0530

    Time: 327 ms (38.45%), Space: 29.9 MB (74.95%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -17,46 +17,40 @@ private:
     // }
     int n,m;
     bool done(int x,int y){
-        return (x==0||x==n-1||y==0||y==m-1);
+        return (x==0||x==n-1||y==m-1||y==0);
     }
     bool check(int x,int y){
         return (x<n&&y<m&&x>=0&&y>=0);
     }
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-         n=maze.size();m=maze[0].size();
-        // vector<vector<bool>>vis(n,vector<bool>(m,0));
-        // vis[entrance[0]][entrance[1]]=1;
-        // exit(maze,vis,entrance[0],entrance[1],0);
-        // return mincnt;
-        //if(done(entrance[0],entrance[1])){
-            maze[entrance[0]][entrance[1]]='+';
-       // }
-        int dx[4]={-1,0,1,0};
-        int dy[4]={0,1,0,-1};
+        n=maze.size();
+        m=maze[0].size();
+        maze[entrance[0]][entrance[1]]='+';
+        int dx[4]={-1,1,0,0};
+        int dy[4]={0,0,1,-1};
         queue<pair<int,int>>pq;
+        int cnt=0;
         pq.push({entrance[0],entrance[1]});
-        int ans=0;
         while(!pq.empty()){
-            int s=pq.size();
-            for(int i=0;i<s;i++){
+            int size=pq.size();
+            for(int i=0;i<size;i++){
                 auto node=pq.front();
                 int x=node.first;
                 int y=node.second;
                 pq.pop();
                 if(done(x,y)&&maze[x][y]!='+'){
-                    return ans;
+                    return cnt;
                 }
                 for(int i=0;i<4;i++){
-                    int nr=x+dx[i],nc=y+dy[i];
+                    int nr=dx[i]+x;int nc=dy[i]+y;
                     if(check(nr,nc)&&maze[nr][nc]=='.'){
                         pq.push({nr,nc});
-                        //cout<<nr<<" "<<nc<<endl;
-                        maze[nr][nc]='*';
+                        maze[nr][nc]='-00';
                     }
                 }
             }
-            ans++;
+            cnt++;
         }
         return -1;
     }
