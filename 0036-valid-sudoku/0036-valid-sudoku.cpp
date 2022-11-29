f5cd89ba2b568d25e37844db2f2fccc510904cdf
sanket gode <88069272+SGGODE@users.noreply.github.com>
2022-11-29 11:01:27 +0530

    Time: 19 ms (96.12%), Space: 19.5 MB (42.13%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,29 +1,10 @@
 class Solution {
-public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        for(int i=0;i<9;i++){
-            map<int,int>mp;
-            for(int j=0;j<9;j++){
-                if(board[i][j]=='.')continue;
-                if(mp.find(board[i][j])!=mp.end()){
-                    //cout<<i<<" "<<j<<" "<<board[i][j]<<endl;
-                    return 0;
-                }
-                mp[board[i][j]]++;
-            }map<int,int>mpcol;
-            for(int j=0;j<9;j++){
-                if(board[j][i]=='.')continue;
-                if(mpcol.find(board[j][i])!=mpcol.end()){
-                   // cout<<i<<" "<<j<<" "<<board[i][j]<<endl;
-                    return 0;
-                }
-                mpcol[board[j][i]]++;
-            }
-        }
+private:
+     bool check(vector<vector<char>>& board,int start,int end){
         map<int,int>mapfirst;
         map<int,int>mapsecond;
         map<int,int>mapthired;
-        for(int i=0;i<3;i++){
+        for(int i=start;i<end;i++){
            for(int j=0;j<3;j++){
                 if(board[i][j]=='.')continue;
                if(mapfirst.find(board[i][j])!=mapfirst.end()){
@@ -49,64 +30,36 @@ public:
                mapthired[board[i][j]]++;
            }
         }
-        map<int,int>mapfour;
-        map<int,int>mapfive;
-        map<int,int>mapsix;
-        for(int i=3;i<6;i++){
-           for(int j=0;j<3;j++){
+       return 1; 
+    }
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
+        for(int i=0;i<9;i++){
+            map<int,int>mp;
+            for(int j=0;j<9;j++){
                 if(board[i][j]=='.')continue;
-               if(mapfour.find(board[i][j])!=mapfour.end()){
-                   //cout<<i<<" "<<j<<" "<<board[i][j]<<endl;
-                   return 0;
-               }
-               mapfour[board[i][j]]++;
-           } 
-           for(int j=3;j<6;j++){
-                if(board[i][j]=='.')continue;
-               if(mapfive.find(board[i][j])!=mapfive.end()){
-                  // cout<<i<<" "<<j<<" "<<board[i][j]<<endl;
-                   return 0;
-               }
-               mapfive[board[i][j]]++;
-           }
-           for(int j=6;j<9;j++){
-                if(board[i][j]=='.')continue;
-               if(mapsix.find(board[i][j])!=mapsix.end()){
-                  // cout<<i<<" "<<j<<" "<<board[i][j]<<endl;
-                   return 0;
-               }
-               mapsix[board[i][j]]++;
-           }
+                if(mp.find(board[i][j])!=mp.end()){
+                    //cout<<i<<" "<<j<<" "<<board[i][j]<<endl;
+                    return 0;
+                }
+                mp[board[i][j]]++;
+            }map<int,int>mpcol;
+            for(int j=0;j<9;j++){
+                if(board[j][i]=='.')continue;
+                if(mpcol.find(board[j][i])!=mpcol.end()){
+                   // cout<<i<<" "<<j<<" "<<board[i][j]<<endl;
+                    return 0;
+                }
+                mpcol[board[j][i]]++;
+            }
         }
-        map<int,int>mapseven;
-        map<int,int>mapeight;
-        map<int,int>mapnine;
-        for(int i=6;i<9;i++){
-           for(int j=0;j<3;j++){
-                if(board[i][j]=='.')continue;
-               if(mapseven.find(board[i][j])!=mapseven.end()){
-                   //cout<<i<<" "<<j<<" "<<board[i][j]<<endl;
-                   return 0;
-               }
-               mapseven[board[i][j]]++;
-           } 
-           for(int j=3;j<6;j++){
-                if(board[i][j]=='.')continue;
-               if(mapeight.find(board[i][j])!=mapeight.end()){
-                  // cout<<i<<" "<<j<<" "<<board[i][j]<<endl;
-                   return 0;
-               }
-               mapeight[board[i][j]]++;
-           }
-           for(int j=6;j<9;j++){
-                if(board[i][j]=='.')continue;
-               if(mapnine.find(board[i][j])!=mapnine.end()){
-                  // cout<<i<<" "<<j<<" "<<board[i][j]<<endl;
-                   return 0;
-               }
-               mapnine[board[i][j]]++;
-           }
+        if(!check(board,0,3)){
+            return 0;
         }
+        if(!check(board,3,6)){
+            return 0;
+        }
+        if(!check(board,6,9))return 0;
         return 1;
     }
 };
