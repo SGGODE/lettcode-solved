c155d04a3cd8bf50432a8a9cfe6866885afa2a5b
sanket gode <88069272+SGGODE@users.noreply.github.com>
2022-12-14 09:37:30 +0530

    Time: 5 ms (22.94%), Space: 7.8 MB (50.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -8,12 +8,13 @@ public:
         dp[0]=nums[0];dp[1]=nums[1];
         for(int i=2;i<n;i++){
             int money=0;
+            dp[i]=nums[i];
             for(int j=i-2;j>=0;j--){
-                money=max(money,nums[j]);
+                money=max(money,dp[j]);
             }
-            nums[i]+=money;
+            dp[i]+=money;
         }
-        int m=*max_element(nums.begin(),nums.end());
+        int m=*max_element(dp.begin(),dp.end());
         return m;
     }
 };
