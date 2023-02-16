8ea67a61f93af506689ff40e50b46b3de5196768
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-02-16 10:00:50 +0530

    Time: 8 ms (76.88%), Space: 19 MB (19.79%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -23,10 +23,9 @@ private:
     }
 public:
     int maxDepth(TreeNode* root) {
-      if(root==NULL)
-          return 0;
-       int left=maxDepth(root->left);
-       int right=maxDepth(root->right);
-     return max(left,right)+1;
+      if(root==NULL)return 0;
+      int left=maxDepth(root->left);
+      int right=maxDepth(root->right);
+      return max(left,right)+1;
     }
 };
