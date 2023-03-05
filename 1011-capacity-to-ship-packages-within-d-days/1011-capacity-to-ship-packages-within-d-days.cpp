249f0c680d7ce3864cce20e43b9a1d34008c35dd
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-03-05 18:01:54 +0530

    Time: 77 ms (33.62%), Space: 31.2 MB (6.17%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -17,21 +17,44 @@ private:
 }
 
 public:
-    int shipWithinDays(vector<int>& weights, int days) {
-        int m=*max_element(weights.begin(),weights.end());
-      //  int x=days;
-        if(days==1){
-            int sum=0;
-            for(auto it:weights)sum+=it;
-            return sum;
+    int shipWithinDays(vector<int>&v, int days) {
+        int n=v.size();
+        int s=*max_element(v.begin(),v.end());
+      // //  int x=days;
+      //   if(days==1){
+      //       int sum=0;
+      //       for(auto it:weights)sum+=it;
+      //       return sum;
+      //   }
+      //   int part=1;int n=weights.size();int i=0;
+      //   while(1){
+      //           if(check(weights,i,m,days,n)){
+      //               return m;
+      //           }
+      //       ++m;
+      //   }
+      //   return m;
+       // int s=1;
+        int e=100000000;
+        while(s<e){
+            int mid=(s+e)/2;
+            int total=0;int xsum=0;
+           for(int i=0;i<n;i++){
+             xsum+=v[i];
+             if(xsum>=mid){
+               if(xsum==mid)xsum=0;
+               else xsum=v[i];
+               ++total;
+               }
+           }
+             if(xsum!=0)++total;
+            //if(total==days)return s;
+            if(total>days){
+                s=mid+1;
+            }else{
+                e=mid;
+            }
         }
-        int part=1;int n=weights.size();int i=0;
-        while(1){
-                if(check(weights,i,m,days,n)){
-                    return m;
-                }
-            ++m;
-        }
-        return m;
+        return s;
     }
 };
