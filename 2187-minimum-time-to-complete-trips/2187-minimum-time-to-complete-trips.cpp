7508f52218e9a46ea71bb34c2b06d218d8fb83fa
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-03-07 18:34:03 +0530

    Time: 428 ms (48.59%), Space: 94.4 MB (76.55%) - LeetHub

--- 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
+++ 2187-minimum-time-to-complete-trips/2187-minimum-time-to-complete-trips.cpp
@@ -6,24 +6,24 @@ public:
         if(totalTrips==1){
             return *min_element(time.begin(),time.end());
         }
-        // int cnt=1;int trip=0;
-        // while(trip!=totalTrips){
-        //     for(auto it:time){
-        //         if(cnt==1||it==1){
-        //            if(it==1)++trip;
-        //            // ++trip;
-        //             continue;
-        //         }
-        //         if(it%cnt==0)++trip;
-        //         // cout<<it<<endl;
-        //         // cout<<cnt<<endl;
-        //         // cout<<trip<<endl;
-        //     }
-        //     if(trip!=totalTrips)++cnt;
-        // }
+       //  int cnt=1;int trip=0;
+       //  while(trip!=totalTrips){
+       //      for(auto it:time){
+       //          if(cnt==1||it==1){
+       //             if(it==1)++trip;
+       //             // ++trip;
+       //              continue;
+       //          }
+       //          if(it%cnt==0)++trip;
+       //          // cout<<it<<endl;
+       //          // cout<<cnt<<endl;
+       //          // cout<<trip<<endl;
+       //      }
+       //      if(trip!=totalTrips)++cnt;
+       //  }
        // return cnt;
       long long int s=*min_element(time.begin(),time.end());
-       long long int e=100000000000000;
+      long long int e=100000000000000;
         while(s<e){
           long long int m=(s+e)/2;
           long long int total=0;
