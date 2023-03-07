0bb1b0c3b68d87c9ca8ca16b1ed59ddfae670eaa
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-03-07 10:50:27 +0530

    Added solution - LeetHub

--- Minimal Cost - GFG/minimal-cost.cpp
+++ Minimal Cost - GFG/minimal-cost.cpp
@@ -6,31 +6,32 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int minimizeCost(vector<int>& height, int n, int k) {
+    int minimizeCost(vector<int>& heights, int n, int k) {
         // Code here
-        vector<int>dp(n+1);
-        dp[0]=0;
-        dp[1]=abs(height[0]-height[1]);
-        for(int i=2;i<=min(k,n);i++){
-            int x=INT_MAX;
-            for(int j=0;j<i;j++){
-               if((abs(height[i]-height[j])+dp[j])<x){
-                    x=abs(height[i]-height[j])+dp[j];
-                }
-            }
-            dp[i]=x;
-        }
-        if(k<n){
-        for(int i=k+1;i<n;i++){
-            int x=INT_MAX;int y=0;
-            for(int j=(i-k);j<i;j++){
-                if((abs(height[i]-height[j])+dp[j])<x){
-                    x=abs(height[i]-height[j])+dp[j];
-                }
-            }
-            dp[i]=x;
-        }}
-        return dp[n-1];
+      vector<int>dp(n+1);
+   dp[0]=0;
+   dp[1]=abs(heights[0]-heights[1]);
+   for(int i=2;i<=min(n,k);i++){
+     int x=INT_MAX;
+     for(int j=0;j<i;j++){
+       if((abs(heights[i]-heights[j])+dp[j])<x){
+         x=abs(heights[i]-heights[j])+dp[j];
+       }
+     }
+     dp[i]=x;
+   }
+   if(k<n){
+    for(int i=k+1;i<n;i++){
+     int x=INT_MAX;
+     for(int j=(i-k);j<i;j++){
+       if((abs(heights[i]-heights[j])+dp[j])<x){
+         x=abs(heights[i]-heights[j])+dp[j];
+       }
+     }
+     dp[i]=x;
+    }
+   }
+       return dp[n-1];
     }
 };
 
