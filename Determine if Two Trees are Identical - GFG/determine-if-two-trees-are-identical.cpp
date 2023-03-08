65db1a8cb8edee1ab6336397ae3a9a4d573b4bdf
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-03-08 18:52:48 +0530

    Added solution - LeetHub

---
+++ Determine if Two Trees are Identical - GFG/determine-if-two-trees-are-identical.cpp
@@ -0,0 +1,155 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int x){
+        data = x;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+/* A binary tree node
+
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+
+   
+class Solution
+{
+    private:
+     void preorder(Node* root,vector<int>&v){
+         if(root==NULL){
+           // v.push_back(-1);
+            return;
+         }
+         v.push_back(root->data);
+          preorder(root->left,v);
+          if(!root->left)v.push_back(-1);
+         preorder(root->right,v);
+         if(!root->right)v.push_back(-1);
+     }
+    public:
+    //Function to check if two trees are identical.
+    bool isIdentical(Node *r1, Node *r2)
+    {
+        //Your Code here
+    //     if(r1->data!=r2->data)return 0;
+    //   if(r1==NULL&&r2==NULL)return 1;
+    //   if((r1->left==NULL&&r2->left!=NULL)||(r1->right!=NULL&&r2->right==NULL)||(r1->right==NULL&&r2->right!=NULL)||
+    //   (r1->left!=NULL&&r2->left==NULL))return 0;
+    //   if(r1->left==r2->left&&r1->right==r2->right)return 1;
+    //   // if(r1->left->data==r2->left->data){
+    //   // }
+    //   return isIdentical(r1->left,r2->left)&&isIdentical(r1->right,r2->right);;
+    vector<int>v1,v2;
+     preorder(r1,v1);
+     preorder(r2,v2);
+     return v1==v2;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string str, str1;
+        getline(cin, str);
+        Node *rootA = buildTree(str);
+        getline(cin, str1);
+        Node *rootB = buildTree(str1);
+        Solution ob;
+        if (ob.isIdentical(rootA, rootB)) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
