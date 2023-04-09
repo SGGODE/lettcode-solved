a88ce7e456d2652cb255f17cb7f2a36e0a3756df
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-04-10 00:33:32 +0530

    Time: 8 ms (14.48%), Space: 10.6 MB (9.32%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -10,47 +10,21 @@
  * };
  */
 class Solution {
-private:
-   bool issame(TreeNode* p,TreeNode* q){
-       if(p==NULL&&q==NULL)return 1;
-       if(p->right!=NULL&&q->right!=NULL){
-           if(p->right->val==q->right->val)issame(p->right,q->right);
-           else return 0;
-       }
-       if(p->left!=NULL&&q->left!=NULL){
-           if(p->left->val==q->left->val)issame(p->left,q->left);
-           else return 0;
-       }
-       if(!p->right&&q->right)return 0;
-        if(!p->left&&q->left)return 0;
-        if(p->right&&!q->right)return 0;
-        return 0;
-   }
-   void preorder(TreeNode* root,vector<int>&v){
-       if(root==NULL){
-          // v.push_back(0);
-           return;
-       }
-       v.push_back(root->val);
-       if(root->left)preorder(root->left,v);
-       if(!root->left)v.push_back(0);
-       if(root->right)preorder(root->right,v);
-        if(!root->right)v.push_back(0);
-   }
 public:
+    void order(TreeNode* root,vector<int>&v){
+        if(root==NULL){
+            v.push_back(1e8);
+            return;
+        }
+        v.push_back(root->val);
+        order(root->left,v);
+        order(root->right,v);
+    }
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<int>v,x;
-        preorder(p,v);
-        preorder(q,x);
-        for(auto it:v)cout<<it<<" ";
-        cout<<endl;
-        return v==x;
-//         if(p->val!=q->val)return 0;
-//        // if(p->val==q->val)return 1;
-//        // else{
-           
-//             return isSameTree(p->right,q->right)&&isSameTree(p->left,q->left);
-//        // }
-//       // return issame(p,q);
+       vector<int>v1;
+       vector<int>v2;
+        order(p,v1);
+        order(q,v2);
+        return v1==v2;
     }
 };
