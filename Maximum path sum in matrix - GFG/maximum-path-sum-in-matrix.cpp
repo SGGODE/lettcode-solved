3e53c8edd249812741f66bdc0bc5c6fb17846767
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-04-17 10:56:36 +0530

    Added solution - LeetHub

--- Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
+++ Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
@@ -20,41 +20,33 @@ private:
     //     int diagonal=pathsum(grid,dp,i-1,j-1);
     //     return dp[i][j]=grid[i][j]+min(side,min(up,diagonal));
     // }
-    int n,m;
-    bool check(int i,int j){
-        return i>=0&&j>=0&&i<n&&j<m;
-    }
-    void maxpossible(vector<vector<int>>&grid,vector<vector<int>>&dp,int i,int j){
-        int max_val=INT_MIN;
-        if(check(i-1,j))max_val=max(max_val,grid[i][j]+dp[i-1][j]);
-        if(check(i-1,j+1))max_val=max(max_val,grid[i][j]+dp[i-1][j+1]);
-        if(check(i-1,j-1))max_val=max(max_val,grid[i][j]+dp[i-1][j-1]);
-        dp[i][j]=max_val;
-    }
+   
 public:
+   int n;
+    bool check(int row,int col){
+        return row>=0&&col>=0&&row<n&&col<n;
+    }
     int maximumPath(int N, vector<vector<int>>grid)
     {
         // code here
-         n=grid.size();
-         m=grid[0].size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        n=N;
+        if(n==1)return grid[0][0];
+        vector<vector<int>>dp(n,vector<int>(n,0));
         for(int i=0;i<1;i++){
-            for(int j=0;j<m;j++){
+            for(int j=0;j<n;j++){
                 dp[i][j]=grid[i][j];
             }
         }
         for(int i=1;i<n;i++){
-            for(int j=0;j<m;j++){
-                maxpossible(grid,dp,i,j);
+            for(int j=0;j<n;j++){
+                int ans=grid[i][j];
+                if(check(i-1,j))ans=max(ans,grid[i][j]+dp[i-1][j]);
+                if(check(i-1,j-1))ans=max(ans,grid[i][j]+dp[i-1][j-1]);
+                if(check(i-1,j+1))ans=max(ans,grid[i][j]+dp[i-1][j+1]);
+                dp[i][j]=ans;
             }
         }
-        int max_sum=INT_MIN;
-        for(int i=n-1;i>n-2;i--){
-            for(int j=0;j<m;j++){
-                max_sum=max(max_sum,dp[i][j]);
-            }
-        }
-        return max_sum;
+        return *max_element(dp[n-1].begin(),dp[n-1].end());
     }
 };
 
