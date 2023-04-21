b08bc030981933d2e01f99fe3152a714be9173f1
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-04-21 23:44:23 +0530

    Added solution - LeetHub

--- Perfect Sum Problem - GFG/perfect-sum-problem.cpp
+++ Perfect Sum Problem - GFG/perfect-sum-problem.cpp
@@ -9,6 +9,7 @@ class Solution{
    int mod=1e9+7;
      int sub(int sum,int arr[],int index,vector<vector<int>>&dp){
        //  if(index==n){
+      // if(sum==0)return 1;
        if(index==0){
              if(sum==0&&arr[0]==0)return 2;
              if(sum==0||arr[0]==sum)return 1;
@@ -21,16 +22,29 @@ class Solution{
          if(sum>=arr[index]){
              p=sub(sum-arr[index],arr,index-1,dp);
          }
-       return dp[index][sum]=(p)%mod+(np)%mod;
+       return dp[index][sum]=(p+np)%mod;
      }
 	public:
 	int perfectSum(int arr[], int n, int sum)
 	{
         // Your code goes here
-        if(n==1&&arr[0]==sum)return 1;
-        if(n==1&&arr[0]!=sum)return 0;
-        vector<vector<int>>dp(n+1,vector<int>(sum+1,-1));
-        sub(sum,arr,n-1,dp);
+        // if(n==1&&arr[0]==sum)return 1;
+        // if(n==1&&arr[0]!=sum)return 0;
+         vector<vector<int>>dp(n,vector<int>(sum+1,-1));
+        // for(int i=0;i<n;i++)dp[i][0]=1;
+        // if(sum>=arr[0])dp[0][arr[0]]=2;
+        // for(int i=1;i<n;i++){
+        //     for(int j=1;j<=sum;j++){
+        //       int np=dp[i-1][j];
+        //       int p=0;
+        //      if(j>=arr[i]){
+        //      p=dp[i-1][j-arr[i]];
+        //     }
+        //     dp[i][j]=p+np;
+        //     }
+        // }
+
+       return sub(sum,arr,n-1,dp);
         // for(auto it:dp){
         //     for(auto vt:it)cout<<vt<<" ";
         //     cout<<endl;
