d215b2ef2f6d152d323d0d1602f9adca2e941b77
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-04-23 21:59:05 +0530

    Time: 5 ms (32.07%), Space: 6.7 MB (35.38%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,12 +1,12 @@
 class Solution {
 private:
     int fun(vector<int>&nums,int sum,vector<int>&dp){
-     //   if(sum==0)return 1;
-        if(sum<0)return 0;
+    
         if(sum==0)return 1;
         if(dp[sum]!=-1)return dp[sum];
         int cnt=0;
         for(int i=0;i<nums.size();i++){
+            if(nums[i]<=sum)
             cnt+=fun(nums,sum-nums[i],dp);
         }
         return dp[sum]=cnt;
