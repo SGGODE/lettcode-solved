1d061e3663358fb706472f8d57183fb34a2f3b16
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-04-26 14:17:31 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (35.97%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -3,15 +3,17 @@ public:
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1)return nums[0];
-        vector<int>dp(n,-1);
+        vector<int>dp(n,0);
         dp[0]=nums[0];
         dp[1]=nums[1];
         for(int i=2;i<n;i++){
-            int ans=INT_MIN;
+            int maxprofit=INT_MIN;
             for(int j=i-2;j>=0;j--){
-                ans=max(ans,nums[i]+dp[j]);
+                if(nums[i]+dp[j]>maxprofit){
+                    maxprofit=nums[i]+dp[j];
+                }
             }
-            dp[i]=ans;
+            dp[i]=maxprofit;
         }
         return *max_element(dp.begin(),dp.end());
     }
