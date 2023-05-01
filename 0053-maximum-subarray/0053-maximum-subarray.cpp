5d643deb218e0b17089c7447166c9918a0d254ee
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-05-01 11:39:14 +0530

    Time: 131 ms (13.62%), Space: 67.7 MB (62.17%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxsum=INT_MIN;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            maxsum=max(sum,maxsum);
+            if(sum<0)sum=0;
+        }
+        return maxsum;
+    }
+};
