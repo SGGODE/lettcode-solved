de91adb4c405b865f8742152358c7a4716d7c6a7
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-05-05 23:13:47 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (37.42%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,37 +1,37 @@
 class Solution {
 private:
-    bool safe(int row,int col,vector<string>&board,int n){
-        int temprow=row;
-        int tempcol=col;
-        while(tempcol>=0){
-            if(board[row][tempcol]=='Q')return 0;
-            --tempcol;
-        }
-        tempcol=col;
-        while(temprow>=0&&tempcol>=0){
-             if(board[temprow][tempcol]=='Q')return 0;
-            --tempcol;
-            --temprow;
-        }
-        temprow=row;
-        tempcol=col;
-        while(temprow<n&&tempcol>=0){
-             if(board[temprow][tempcol]=='Q')return 0;
-            --tempcol;
-            ++temprow;
-        }
-        return 1;
-    }
-    void solve(int col,vector<string>&board,set<vector<string>>&res,int n){
+    // bool safe(int row,int col,vector<string>&board,int n){
+    //     int temprow=row;
+    //     int tempcol=col;
+    //     while(tempcol>=0){
+    //         if(board[row][tempcol]=='Q')return 0;
+    //         --tempcol;
+    //     }
+    //     tempcol=col;
+    //     while(temprow>=0&&tempcol>=0){
+    //          if(board[temprow][tempcol]=='Q')return 0;
+    //         --tempcol;
+    //         --temprow;
+    //     }
+    //     temprow=row;
+    //     tempcol=col;
+    //     while(temprow<n&&tempcol>=0){
+    //          if(board[temprow][tempcol]=='Q')return 0;
+    //         --tempcol;
+    //         ++temprow;
+    //     }
+    //     return 1;
+    // }
+    void solve(int col,vector<string>&board,set<vector<string>>&res,int n,vector<int>&rowcheck,vector<int>&lowerdia,vector<int>&upperdia){
         if(col==n){
             res.insert(board);
             return;
         }
         for(int row=0;row<n;row++){
-            if(safe(row,col,board,n)){
-                board[row][col]='Q';
-                solve(col+1,board,res,n);
-                board[row][col]='.';
+            if(rowcheck[row]==0&&lowerdia[row+col]==0&&upperdia[n-1+col-row]==0){
+                board[row][col]='Q';rowcheck[row]=1;lowerdia[row+col]=1;upperdia[n-1+col-row]=1;
+                solve(col+1,board,res,n,rowcheck,lowerdia,upperdia);
+                board[row][col]='.';rowcheck[row]=0;lowerdia[row+col]=0;upperdia[n-1+col-row]=0;
             }
         }
     }
@@ -43,7 +43,9 @@ public:
             board[i]=s;
         }
         set<vector<string>>res;
-        solve(0,board,res,n);
+        vector<int>rowcheck(n,0);
+        vector<int>lowerdia(2*n-1,0),upperdia(2*n-1,0);
+        solve(0,board,res,n,rowcheck,lowerdia,upperdia);
         return res.size();
     }
 };
