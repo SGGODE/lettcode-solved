d2e787186b7451de2bc786903a62297a1304fbaf
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-05-12 10:31:56 +0530

    Time: 378 ms (57.53%), Space: 115.1 MB (93.26%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,36 @@
+class Solution {
+private:
+   // int mod=1e5+7;
+   // int maxsum=INT_MIN;
+    int solve(vector<vector<int>>& questions,long long int index,int n,int sum,vector<long long int>&dp){
+        if(index>=n){
+            //maxsum=max(maxsum,sum);
+            return 0;
+        }
+        if(dp[index]!=-1)return dp[index];
+        long long int notpick =(long long) 0 + solve(questions,index+1,n,sum,dp);
+        long long int pick =(long long) questions[index][0] + solve(questions,index+questions[index][1]+1,n,sum,dp);
+        return dp[index]=max(notpick,pick);
+    }
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+       int n=questions.size();
+       vector<long long int>dp(n,-1);
+       //solve(questions,0,n,0,dp);
+        long long int res=INT_MIN;
+        for(int i=n-1;i>=0;i--){
+           long long int sum=questions[i][0];
+            if((i+questions[i][1]+1)>=n){
+                if(i!=n-1)dp[i]=max(dp[i+1],sum);
+                else dp[i]=sum;
+            }else{
+                if(i!=n-1)dp[i]=max(dp[i+1],sum+dp[(i+questions[i][1]+1)]);
+                else dp[i]=sum+dp[(i+questions[i][1]+1)];
+            }
+           res=max(res,dp[i]);
+        }
+        return res;
+        return *max_element(dp.begin(),dp.end());
+      // return maxsum;
+    }
+};
