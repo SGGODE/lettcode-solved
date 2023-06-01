93b307a89a16e97d882620fd9cf46da813e49aa1
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-06-01 10:29:41 +0530

    Time: 94 ms (38.12%), Space: 22 MB (28.56%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,38 +1,38 @@
 class Solution {
 private:
-    bool check(int row,int col,int dim){
-        return row>=0&&col>=0&&col<dim&&row<dim;
+    int n,m;
+    bool check(int row,int col){
+        return row>=0&&col>=0&&row<n&&col<m;
     }
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int n=grid.size();
-        if(grid[0][0]==1||grid[n-1][n-1]==1)return -1;
-        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
-        pq.push({1,{0,0}});
-        vector<vector<bool>>vis(n,vector<bool>(n,0));
-        vis[0][0]=1;
-        vector<vector<int>>res(n,vector<int>(n,1e9));
+        n=grid.size();
+        m=grid[0].size();
+        if(grid[0][0]==1)return -1;
+        queue<pair<pair<int,int>,int>>pq;
+        pq.push({{0,0},1});
+        int dx[8]={0,1,0,-1,-1,-1,1,1};
+        int dy[8]={1,0,-1,0,-1,1,-1,1};
+        int minstep=INT_MAX;
+        vector<vector<bool>>vis(n,vector<bool>(m,0));
+        vector<vector<int>>res(n,vector<int>(m,1e8));
         res[0][0]=1;
-        int min_cost=INT_MAX;
         while(!pq.empty()){
-            int row=pq.top().second.first;
-            int col=pq.top().second.second;
-            int cost=pq.top().first;
+            int row=pq.front().first.first;
+            int col=pq.front().first.second;
+            int dist=pq.front().second;
             pq.pop();
             vis[row][col]=1;
-            if(row==n-1&&col==n-1){
-                min_cost=min(min_cost,cost);
+            if(row==n-1&&col==m-1){
+                minstep=min(minstep,dist);
+                continue;
             }
-            int dx[8]={0,1,0,-1,-1,1,-1,1};
-            int dy[8]={1,0,-1,0,-1,1,1,-1};
             for(int i=0;i<8;i++){
-                if(check(row+dx[i],col+dy[i],n)&&grid[row+dx[i]][col+dy[i]]==0&&!vis[row+dx[i]][col+dy[i]]&&res[row+dx[i]][col+dy[i]]>
-                  cost+1){
-                    res[row+dx[i]][col+dy[i]]=cost+1;
-                    pq.push({cost+1,{row+dx[i],col+dy[i]}});
+                if(check(row+dx[i],col+dy[i])&&grid[row+dx[i]][col+dy[i]]==0&&!vis[row+dx[i]][col+dy[i]]&&res[row+dx[i]][col+dy[i]]>(res[row][col]+1)){         res[row+dx[i]][col+dy[i]]=res[row][col]+1;
+                    pq.push({{row+dx[i],col+dy[i]},dist+1});
                 }
             }
         }
-        return min_cost==INT_MAX?-1:min_cost;
+        return minstep==INT_MAX?-1:minstep;
     }
 };
