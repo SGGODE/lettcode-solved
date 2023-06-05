f9cb2eaa8e4099da6172c225deb871a4fa831641
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-06-05 17:02:56 +0530

    Added solution - LeetHub

--- Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
+++ Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
@@ -8,42 +8,27 @@ using namespace std;
 // User function Template for C++
 
 class Solution{
-private:
-    // int pathsum(vector<vector<int>>&grid,vector<vector<int>>&dp,int i,int j){
-    //     if(i==0&&j==0){
-    //         return grid[i][j];
-    //     }
-    //     if(i<0||j<0)return INT_MAX;
-    //     if(dp[i][j]!=-1)return dp[i][j];
-    //     int up=pathsum(grid,dp,i-1,j);
-    //     int side=pathsum(grid,dp,i,j-1);
-    //     int diagonal=pathsum(grid,dp,i-1,j-1);
-    //     return dp[i][j]=grid[i][j]+min(side,min(up,diagonal));
-    // }
-   
 public:
-   int n;
-    bool check(int row,int col){
-        return row>=0&&col>=0&&row<n&&col<n;
-    }
-    int maximumPath(int N, vector<vector<int>>grid)
+   bool check(int row,int col,int n){
+       return row>=0&&col>=0&&row<n&&col<n;
+   }
+    int maximumPath(int n, vector<vector<int>> Matrix)
     {
         // code here
-        n=N;
-        if(n==1)return grid[0][0];
         vector<vector<int>>dp(n,vector<int>(n,0));
         for(int i=0;i<1;i++){
             for(int j=0;j<n;j++){
-                dp[i][j]=grid[i][j];
+                dp[i][j]=Matrix[i][j];
             }
         }
         for(int i=1;i<n;i++){
             for(int j=0;j<n;j++){
-                int ans=grid[i][j];
-                if(check(i-1,j))ans=max(ans,grid[i][j]+dp[i-1][j]);
-                if(check(i-1,j-1))ans=max(ans,grid[i][j]+dp[i-1][j-1]);
-                if(check(i-1,j+1))ans=max(ans,grid[i][j]+dp[i-1][j+1]);
-                dp[i][j]=ans;
+                int x=-1e9,y=-1e9,z=-1e9;
+                if(check(i-1,j,n))x=dp[i-1][j];
+                if(check(i-1,j-1,n))y=dp[i-1][j-1];
+                if(check(i-1,j+1,n))z=dp[i-1][j+1];
+                dp[i][j]=max(x,max(y,z));
+                dp[i][j]+=Matrix[i][j];
             }
         }
         return *max_element(dp[n-1].begin(),dp[n-1].end());
