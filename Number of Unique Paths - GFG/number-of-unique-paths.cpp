e57f7434693fc40df56633bed885dd99f1267a5a
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-06-05 16:29:35 +0530

    Added solution - LeetHub

--- Number of Unique Paths - GFG/number-of-unique-paths.cpp
+++ Number of Unique Paths - GFG/number-of-unique-paths.cpp
@@ -6,44 +6,39 @@ using namespace std;
 // } Driver Code Ends
 //User function template in C++
 
-class Solution
-{
-    public:
+class Solution{
+    private:
     bool check(int row,int col,int n,int m){
         return row>=0&&col>=0&&row<n&&col<m;
     }
-    int check(vector<vector<int>>&dp,int a,int b){
-        if(a<0||b<0)return 0;
-        if(dp[a][b]!=-1)return dp[a][b];
-        return dp[a][b]=check(dp,a-1,b)+check(dp,a,b-1);
+    int dfs(vector<vector<int>>&dp,int row,int col,int n,int m){
+        if(row==n-1&&col==m-1)return 1;
+        if(dp[row][col]!=-1)return dp[row][col];
+        int down=0;
+        int right=0;
+        if(check(row+1,col,n,m))down=dfs(dp,row+1,col,n,m);
+        if(check(row,col+1,n,m))right=dfs(dp,row,col+1,n,m);
+        return dp[row][col]=down+right;
     }
+    public:
     //Function to find total number of unique paths.
     int NumberOfPath(int a, int b)
     {
         //code here
-        vector<vector<int>>dp(a+1,vector<int>(b+1,-1));
-        dp[0][0]=1;
-        dp[0][1]=1;
-        dp[1][0]=1;
-        if(a<1&&b<1)return dp[a][b];
-        check(dp,a,b);
-        return dp[a-1][b-1];
-        // queue<pair<int,int>>pq;
+       // int cnt=0;
+       // queue<pair<int,int>>pq;
+        vector<vector<int>>dp(a,vector<int>(b,-1));
+        return dfs(dp,0,0,a,b);
         // pq.push({0,0});
-        // int cnt=0;
         // while(!pq.empty()){
         //     int row=pq.front().first;
         //     int col=pq.front().second;
         //     pq.pop();
         //     if(row==a-1&&col==b-1)++cnt;
-        //     if(check(row+1,col,a,b)){
-        //         pq.push({row+1,col});
-        //     }
-        //     if(check(row,col+1,a,b)){
-        //         pq.push({row,col+1});
-        //     }
+        //     if(check(row+1,col,a,b))pq.push({row+1,col});
+        //     if(check(row,col+1,a,b))pq.push({row,col+1});
         // }
-        // return cnt;
+        //return dp[a-1][b-1];
     }
 };
 
