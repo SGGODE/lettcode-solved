e8ec718b921c01468cbb4bda05261a6b4831ace1
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-06-06 16:09:09 +0530

    Time: 136 ms (38.96%), Space: 45.6 MB (27.99%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,24 +1,37 @@
 class Solution {
 private:
-    int fun(vector<int>&coins,int index,int sum,vector<vector<int>>&dp){
+    int mincnt=INT_MAX;
+    int combination(vector<int>&coins,vector<vector<int>>&dp,int sum,int index,int cnt){
+        // if(sum==0){
+        //    // cout<<cnt<<endl;
+        //     mincnt=min(mincnt,cnt);
+        //     return 1;
+        // }
         if(index==0){
-            if(sum%coins[0]==0)return sum/coins[0];
+            if(sum%coins[index]==0){
+                // cnt+=(sum/coins[index]);
+                // mincnt=min(mincnt,cnt);
+                return sum/coins[index];
+            }
+            //return sum%coins[index]==0;
             return 1e9;
         }
         if(dp[index][sum]!=-1)return dp[index][sum];
-        int notpick=0+fun(coins,index-1,sum,dp);
-        int pick=INT_MAX;
+        int notpick=0+combination(coins,dp,sum,index-1,cnt);
+        int pick=1e9;
         if(sum>=coins[index]){
-            pick=1+fun(coins,index,sum-coins[index],dp);
+            pick=1+combination(coins,dp,sum-coins[index],index,cnt+1);
         }
-        return dp[index][sum]=min(pick,notpick);
+        return dp[index][sum]=min(notpick,pick);
     }
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        long long int x = fun(coins,n-1,amount,dp);
-        if(x==1e9)return -1;
-        return x;
+       // return 
+        long long res = combination(coins,dp,amount,n-1,0);
+        if(res==1e9)return -1;
+        return res;
+       // return mincnt==INT_MAX?-1:mincnt;
     }
 };
