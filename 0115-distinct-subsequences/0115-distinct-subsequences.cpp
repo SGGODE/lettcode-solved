bba34a739d32e501152c8514a688bcb0860f4233
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-06-09 15:01:19 +0530

    Time: 2614 ms (12.18%), Space: 422.5 MB (12.37%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,19 +1,22 @@
 class Solution {
 private:
-    int fun(int i,int j,string s,string t,vector<vector<int>>&dp){
-        if((j<0&&i>=0)||(j<0&&i<0))return 1;
-        if(i<0&&j>=0)return 0;
+    int sub(vector<vector<int>>&dp,string s,string t,int i,int j){
+        if(j<0)return 1;
+        if(i<0)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
+        int pick=0;
+        int notpick=0;
         if(s[i]==t[j]){
-            return dp[i][j]=fun(i-1,j-1,s,t,dp)+fun(i-1,j,s,t,dp);
+            pick=sub(dp,s,t,i-1,j-1)+sub(dp,s,t,i-1,j);
         }
-        return dp[i][j]=fun(i-1,j,s,t,dp);
+        else notpick=sub(dp,s,t,i-1,j);
+        return dp[i][j]=pick+notpick;
     }
 public:
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        return fun(n-1,m-1,s,t,dp);
+        return sub(dp,s,t,n-1,m-1);
     }
 };
