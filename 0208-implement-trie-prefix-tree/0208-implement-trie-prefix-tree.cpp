cb3fd4a3b85d80b9442bfe88861243dd9719d686
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-06-20 13:29:37 +0530

    Time: 52 ms (94.76%), Space: 45.1 MB (42.35%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,27 +1,63 @@
+class Node{
+public:
+    Node *links[26];
+    bool flag=0;
+    bool contain(char ch){
+        return links[ch-'a']!=NULL;
+    }
+    void put(char ch,Node* node){
+        links[ch-'a']=node;
+    }
+    Node* get(char ch){
+        return links[ch-'a'];
+    }
+    void setEnd(){
+        flag=1;
+    }
+    bool isend(){
+        return flag;
+    }
+};
+
 class Trie {
+private:
+    Node* root;
 public:
     Trie() {
-        
+        root = new Node();
     }
-    unordered_map<string,int>mp;
+    
     void insert(string word) {
-        mp[word]++;
+        Node* node=root;
+        for(int i=0;i<word.size();i++){
+            if(!node->contain(word[i])){
+                node->put(word[i],new Node());
+            }
+            node =node->get(word[i]);
+        }
+        node->setEnd();
     }
     
     bool search(string word) {
-        return mp.count(word);
+        Node* node=root;
+        for(int i=0;i<word.size();i++){
+            if(!node->contain(word[i])){
+                return 0;
+            }
+            node =node->get(word[i]);
+        }
+        return node->isend();
     }
     
     bool startsWith(string prefix) {
-        for(auto it:mp){
-            string x=it.first;
-            int n=prefix.size();
-           // for(int )
-            string sub=x.substr(0,n);
-            if(prefix==sub)return 1;
-         //   if()
+        Node* node=root;
+        for(int i=0;i<prefix.size();i++){
+            if(!node->contain(prefix[i])){
+                return 0;
+            }
+            node =node->get(prefix[i]);
         }
-        return 0;
+        return 1;
     }
 };
 
