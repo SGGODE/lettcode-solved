f1f8831236425db46a7570911de7ac1f1560591e
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-06-23 06:49:37 +0530

    Time: 16 ms (19.09%), Space: 15.2 MB (21.24%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -20,8 +20,20 @@ public:
         // if(nums[0]==397&&nums[n-1]==6784)return 1697678;
         // if(nums[0]==10000&&nums[n-1]==0)return 4;
         vector<int>res;
-        vector<vector<int>>dp(n,vector<int>((2),-1));
-        return sub(dp,nums,res,0,n,1);
+        vector<vector<int>>dp(n+1,vector<int>((2),0));
+        for(int i=n-1;i>=0;i--){
+            for(int j=0;j<=1;j++){
+                  int profit=0;
+                 if(j){
+                      profit=max(-nums[i]+dp[i+1][0],dp[i+1][1]); 
+                 }else{
+                       profit=max(nums[i]+dp[i+1][1],dp[i+1][0]); 
+                 }
+                 dp[i][j]=profit;
+            }
+        }
+        return dp[0][1];
+        // return sub(dp,nums,res,0,n,1);
        // return totalsum;
     }
 };
