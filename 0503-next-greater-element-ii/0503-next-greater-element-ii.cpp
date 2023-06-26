b22ab3cafa56cfc70ec1a8df4d0df60131e63294
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-06-26 21:27:49 +0530

    Time: 41 ms (55.44%), Space: 24.4 MB (30.07%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -3,23 +3,35 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n=nums.size();
         vector<int>res(n,-1);
-        for(int i=0;i<n;i++){
-            bool flag=0;
-            for(int j=i+1;j<n;j++){
-                if(nums[j]>nums[i]){
-                    res[i]=nums[j];
-                    flag=1;break;
-                }
-            }
-            if(!flag){
-                for(int j=0;j<i;j++){             
-                    if(nums[j]>nums[i]){
-                    res[i]=nums[j];
-                    flag=1;break;
-                }
-                }
+        // for(int i=0;i<n;i++){
+        //     bool flag=0;
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[j]>nums[i]){
+        //             res[i]=nums[j];
+        //             flag=1;break;
+        //         }
+        //     }
+        //     if(!flag){
+        //         for(int j=0;j<i;j++){
+        //         if(nums[j]>nums[i]){
+        //             res[i]=nums[j];
+        //             flag=1;break;
+        //         }             
+        //         }
+        //     }
+        // }
+        stack<int>s;
+        vector<int>next(n,-1);
+        for(int i=(2*n)-1;i>=0;i--){
+            if(s.empty()){
+                s.push({nums[i%n]});
+            }else{
+                while(!s.empty()&&s.top()<=nums[i%n])s.pop();
+                if(!s.empty())next[i%n]=s.top();
+                s.push(nums[i%n]);
             }
         }
+        for(int i=0;i<n;i++)res[i]=next[i];
         return res;
     }
 };
