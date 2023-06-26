152f24e23d1f7c3fbb35f98f6e7be77e7a056a18
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-06-26 21:23:56 +0530

    Time: 8 ms (51.39%), Space: 9.1 MB (6.57%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,20 +4,32 @@ public:
         int n=nums2.size();
         int m=nums1.size();
         map<int,int>mp;
+        stack<int>s;
+        vector<int>next(n,-1);
+        for(int i=n-1;i>=0;i--){
+            if(s.empty()){
+                s.push({nums2[i]});
+            }else{
+                while(!s.empty()&&s.top()<nums2[i])s.pop();
+                if(!s.empty())next[i]=s.top();
+                s.push(nums2[i]);
+            }
+        }
         for(int i=0;i<n;i++){
-            if(!mp.count(nums2[i]))mp[nums2[i]]=i;
+            if(!mp.count(nums2[i]))mp[nums2[i]]=next[i];
         }
         vector<int>res(m,-1);
         for(int i=0;i<m;i++){
-            bool flag=0;
-            for(int j=0;j<n;j++){
-                if(nums2[j]==nums1[i])flag=1;
-                if(flag&&nums2[j]>nums1[i]){
-                    res[i]=nums2[j];
-                    break;
-                }
-            }
+            res[i]=mp[nums1[i]];
         }
+        // for(int i=0;i<m;i++){
+        //     for(int j=mp[nums1[i]]+1;j<n;j++){
+        //         if(nums2[j]>nums1[i]){
+        //             res[i]=nums2[j];
+        //             break;
+        //         }
+        //     }
+        // }
         return res;
     }
 };
