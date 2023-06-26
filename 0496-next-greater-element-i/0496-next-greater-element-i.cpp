8bb484bcb4f997edbc728b296fb5ee81a2636116
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-06-26 20:29:56 +0530

    Time: 8 ms (51.39%), Space: 9 MB (33.33%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums2.size();
+        int m=nums1.size();
+        map<int,int>mp;
+        for(int i=0;i<n;i++){
+            if(!mp.count(nums2[i]))mp[nums2[i]]=i;
+        }
+        vector<int>res(m,-1);
+        for(int i=0;i<m;i++){
+            for(int j=mp[nums1[i]]+1;j<n;j++){
+                if(nums2[j]>nums1[i]){
+                    res[i]=nums2[j];
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+};
