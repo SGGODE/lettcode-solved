5d64319e147b79836a53e62ea2bd0b65225b72a4
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-06-27 09:36:01 +0530

    Time: 408 ms (23.38%), Space: 110 MB (68.23%) - LeetHub

---
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        vector<vector<int>>res;
+        int n=nums1.size();
+        int m=nums2.size();
+         priority_queue<pair<int,pair<int,int>>>pq;
+        // map<int,int>mp;
+        // for(auto&it:nums1)mp[it]++;
+        // nums1.clear();
+        // for(auto&it:mp)nums1.push_back(it.first);
+        // sort(nums1.begin(),nums1.end());
+        for(int i=0;i<nums1.size();i++){
+            for(int j=0;j<nums2.size();j++){
+                // int temp=mp[nums1[i]];
+                // while(temp--){
+                 if(pq.size()<k)pq.push({nums1[i]+nums2[j],{nums1[i],nums2[j]}});
+                 else if(pq.top().first>nums1[i]+nums2[j]){
+                     pq.pop();
+                     pq.push({nums1[i]+nums2[j],{nums1[i],nums2[j]}});
+                 }else break;
+                   // --k;
+               // }
+            }
+        }
+        // priority_queue<int>pqx,pqy;
+        // for(int i=0;i<min(n,k);i++){
+        //     pqx.push({nums1[i]});
+        //     if(pqx.size()>k)pqx.pop();
+        // }
+        // for(int i=0;i<min(m,k);i++){
+        //     pqy.push({nums2[i]});
+        //     if(pqy.size()>k)pqy.pop();
+        // }
+        // priority_queue<int,vector<int>,greater<int>>pq1,pq2;
+        // while(!pqx.empty()){
+        //     pq1.push(pqx.top());
+        //     pqx.pop();
+        // }
+        //  while(!pqy.empty()){
+        //     pq2.push(pqy.top());
+        //     pqy.pop();
+        // }
+        // priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        // while(!pq1.empty()){
+        //     priority_queue<int,vector<int>,greater<int>>pqx(pq2);
+        //     while(!pqx.empty()){
+        //         pq.push({pq1.top()+pqx.top(),{pq1.top(),pqx.top()}});
+        //        // if(pq.size()>k)pq.pop();
+        //         pqx.pop();
+        //     }
+        //     pq1.pop();
+        // }
+         while(!pq.empty()&&k--){
+            res.push_back({pq.top().second.first,pq.top().second.second});
+            pq.pop();
+        }
+        return res;
+    }
+};
