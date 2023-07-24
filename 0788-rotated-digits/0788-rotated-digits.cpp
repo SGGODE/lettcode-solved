0f83d9504e21365ef5a394ed1e688d78b791aba8
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-07-24 09:27:49 +0530

    Time: 21 ms (34.52%), Space: 6 MB (34.52%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.cpp
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -5,7 +5,6 @@ private:
         bool flag=1;
         for(int i=0;i<res.size();i++){
             if(res[i]-'0'==1||res[i]-'0'==0||res[i]-'0'==8){
-              //  continue;
                int a = 1;
             }else if(res[i]-'0'==2||res[i]-'0'==5||res[i]-'0'==6||res[i]-'0'==9){
                if(res[i]-'0'==2){
@@ -20,15 +19,11 @@ private:
                    continue;
                } if(res[i]-'0'==9){
                    res[i]='6';
-                   //continue;
                } 
             }else{
-                res[i]='-1';
-                flag=0;
-                break;
+               return 0;
             }
         }
-        if(!flag)return 0;
         return stoi(res)!=x;
     }
 public:
@@ -36,7 +31,6 @@ public:
         int cnt=0;
         for(int i=1;i<=n;i++){
             if(good(i)){
-                //cout<<i<<endl;
                 ++cnt;
             }
         }
