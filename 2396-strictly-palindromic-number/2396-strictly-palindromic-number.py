bf599226c2f1ab76bd9e618c62c6ab51d73e17ad
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-08-07 18:45:17 +0530

    Time: 24 ms (25.50%), Space: 16.3 MB (33.00%) - LeetHub

---
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -0,0 +1,40 @@
+# class Solution(object):
+#     def check(x):
+#         time=0
+#         while time<len(x)/2
+#            if x[time]==x[len(x)-time-1]:
+#               time+=1
+#            else:
+#             return 0
+          
+        
+#         return 1
+        
+#     def isStrictlyPalindromic(self, n):
+#         """
+#         :type n: int
+#         :rtype: bool
+#         """
+#         for x in (3,n-2):
+#             if !check(bin(x)):
+#                 return 0
+            
+#         return 1
+class Solution(object):
+    def check(self, x):
+        time = 0
+        while time < len(x) / 2:
+            if x[time] == x[len(x) - time - 1]:
+                time += 1
+            else:
+                return 0
+        return 1
+
+    def isStrictlyPalindromic(self, n):
+        for x in range(2, n - 1):  
+            if not self.check(str(bin(x)[2:])): 
+                print(str(bin(x)[2:]))
+                return False
+        return True  
+
+        
