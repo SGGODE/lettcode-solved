6ef249d6ff0b9e93356092fbd3753fd8b7ed262f
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-08-08 10:41:42 +0530

    Time: 7 ms (28.91%), Space: 11.1 MB (13.70%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-       map<int,int>mp;
-       for(int i=0;i<nums.size();i++){
-           mp[nums[i]]=i;
-       }
-       sort(nums.begin(),nums.end());
+       // map<int,int>mp;
+       // for(int i=0;i<nums.size();i++){
+       //     mp[nums[i]]=i;
+       // }
+       // sort(nums.begin(),nums.end());
        int n = nums.size();
-       int s = 0;
-       int e = n-1;
-       while(s<=e){
-           int mid = (s+e)/2;
-           if(nums[mid]==target)return mp[nums[mid]];
-           if(nums[mid]>target){
-               e=mid-1;
-           }else{
-               s=mid+1;
-           }
-       }
+       // int s = 0;
+       // int e = n-1;
+       // while(s<=e){
+       //     int mid = (s+e)/2;
+       //     if(nums[mid]==target)return mp[nums[mid]];
+       //     if(nums[mid]>target){
+       //         e=mid-1;
+       //     }else{
+       //         s=mid+1;
+       //     }
+       // }
+       //  return -1;
+        int s = 0;
+        int e = n-1;
+        while(s<=e){
+            int mid = (s+e)/2;
+            if(nums[mid]==target) return mid;
+            if(nums[mid]>=nums[s]){
+               if(target>=nums[s]&&target<nums[mid]){
+                   e=mid-1;
+               }else{
+                   s=mid+1;
+               }
+            }else{
+                if(target>nums[mid]&&target<=nums[e]){
+                   s=mid+1;
+               }else{
+                   e=mid-1;
+               }
+            }
+        }
         return -1;
     }
 };
