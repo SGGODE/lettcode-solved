a9ac58e34bc4000546c3aa24aeb3428a05b70ad5
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-08-11 10:02:07 +0530

    Time: 32 ms (50.43%), Space: 18.7 MB (29.68%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,29 +1,24 @@
 class Solution {
 private:
-    int combinations(vector<int>&coins,vector<vector<int>>&dp,int sum,int index){
-        if(sum==0)return 1;
-        if(index==0){
-            if(sum%coins[index]==0)return 1;
+    int sub(vector<vector<int>>&dp,vector<int>&nums,int sum,int index){
+        if(sum==0){
+            return 1;
+        }
+        if(index==nums.size()){
             return 0;
         }
-       // cout<<index<<endl;
         if(dp[index][sum]!=-1)return dp[index][sum];
-        int notpick=0+combinations(coins,dp,sum,index-1);
+        int notpick=sub(dp,nums,sum,index+1);
         int pick=0;
-        if(sum>=coins[index]){
-            pick=combinations(coins,dp,sum-coins[index],index);
+        if(sum>=nums[index]){
+            pick=sub(dp,nums,sum-nums[index],index);
         }
         return dp[index][sum]=pick+notpick;
     }
 public:
     int change(int amount, vector<int>& coins) {
-        int n=coins.size();
+        int n = coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-       return combinations(coins,dp,amount,n-1);
-        // for(auto &it:dp){
-        //     for(auto &vt:it)cout<<vt<<" ";
-        //     cout<<endl;
-        // }
-        // return dp[0][0];
+        return sub(dp,coins,amount,0);
     }
 };
