e72953790fa3e01f84135392d0a4b8382381d0af
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-08-16 11:01:11 +0530

    Time: 213 ms (83.39%), Space: 148.8 MB (24.08%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
-    vector<int> maxSlidingWindow(vector<int>& arr, int k) {
-        vector<int>v;
-        int n=arr.size();
-        int m=0;//map<int,int>mp;
-        multiset<int>ms;
-        //priority_queue<int>pq;
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        // vector<int>res;
+        // deque<int>s;
+        // //deque<int>::iterator maxElementIt = max_element(myDeque.begin(), myDeque.end());
+        // for(int i=0;i<nums.size();i++){
+        //     s.push_back(nums[i]);
+        //     if(s.size()==k){                                                                                                                                   
+        //         deque<int>::iterator it = max_element(s.begin(), s.end());
+        //         res.push_back(*it);
+        //     }
+        //     if(s.size()>k){
+        //         s.pop_front();
+        //         deque<int>::iterator it = max_element(s.begin(), s.end());
+        //         res.push_back(*it);
+        //     }
+        // }
+        // return res;
+        vector<int>res;
+        priority_queue<pair<int,int>>pq;
         for(int i=0;i<k;i++){
-            m=max(m,arr[i]);
-           // mp[arr[i]]++;
-            //pq.push(arr[i]);
-            ms.insert(arr[i]);
+            pq.push({nums[i],i});
         }
-        v.push_back(m);
-        for(int i=k;i<n;i++){
-         ms.insert(arr[i]);
-         ms.erase(ms.find(arr[i-k]));
-         int x=*(ms.rbegin());
-          v.push_back(x);
+        res.push_back(pq.top().first);
+        for(int i=k;i<nums.size();i++){
+            pq.push({nums[i],i});
+            // if(pq.size()==k){
+            //     res.push_back(pq.top().first);
+            // }
+            //if(pq.size()>k){
+                while(!(pq.top().second>(i-k))){
+                    pq.pop();
+                }
+                 res.push_back(pq.top().first);
+            //}
         }
-        return v;
+        return res;
     }
 };
