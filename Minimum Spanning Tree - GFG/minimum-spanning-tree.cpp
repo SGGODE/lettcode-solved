dce7669fcc26bb19e6106bd8a9c8ae0fa1b3d4d5
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-08-19 11:26:41 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -7,39 +7,27 @@ class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[]){
-        vector<pair<long long int,long long int>>dump[V+1];
-        for(int i=0;i<V;i++){
-          for(auto it:adj[i]){
-              dump[i].push_back({it[0],it[1]});
-              dump[it[0]].push_back({i,it[1]});
-          }
-        }
-        vector<long long int>curr[V+1];
-        priority_queue<pair<long long int,long long int>,vector<pair<long long int,long long int>>,greater<pair<long long int,long long int>>>pq;
-        for(auto it:dump[0]){
-            pq.push({it.second,it.first});
-        }
-        vector<bool>vis(V,0);
-       long long int sum=0;
-        vis[0]=1;
-        while(!pq.empty()){
-           long long int temp=pq.top().second;
-           long long int xum=pq.top().first;
-           if(vis[temp]){
-               pq.pop();continue;
-           }
-            pq.pop();
-            vis[temp]=1;
-            for(auto it:dump[temp]){
-                if(vis[it.first])continue;
-                else{
-                    pq.push({it.second,it.first});
-                }
-            }
-           sum+=xum;
-        }
-        return sum;
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        // code here
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+    pq.push({0,0});
+    int sum=0;
+    vector<bool>vis(V,0);
+    while(!pq.empty()){
+      int node = pq.top().second;
+      int dist = pq.top().first;
+      pq.pop();
+      if(vis[node]==1)continue;
+      sum+=dist;
+      vis[node]=1;
+      for(auto&it:adj[node]){
+        if(!vis[it[0]]){
+          pq.push({it[1],it[0]});
+       }
+     }
+   }
+     return sum;
     }
 };
 
