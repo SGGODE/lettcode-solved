95528931eef246f1e1336b3e32276e8b64acec54
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-08-21 14:50:13 +0530

    Time: 108 ms (74.80%), Space: 51.6 MB (27.48%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,85 +1,48 @@
-// class Disjointset{
-//   vector<int>parent,rank;
-//    public:
-//     Disjointset(int n){
-//         parent.resize(n+1);
-//         rank.resize(n+1,0);
-//         for(int i=0;i<=n;i++){
-//             parent[i]=i;
-//         }
-//     }
-//     int par(int root){
-//         if(root==parent[root])return root;
-//         return parent[root]=par(parent[root]);
-//     }
-//     void unionrank(int u,int v){
-//         int pu=par(u);
-//         int pv=par(v);
-//         if(pu==pv)return;
-//         else if(rank[pu]>rank[pv]){
-//             parent[pv]=pu;
-//         }
-//         else if(rank[pv]>rank[pu]){
-//             parent[pu]=pv;
-//         }else{
-//             parent[pu]=pv;
-//             rank[pv]++;
-//         }
-//     }
-// };
-class DisjointSet {
-    vector<int> rank, parent, size; 
-public: 
-    DisjointSet(int n) {
-        rank.resize(n+1, 0); 
-        parent.resize(n+1);
-        size.resize(n+1); 
-        for(int i = 0;i<=n;i++) {
-            parent[i] = i; 
-            size[i] = 1; 
-        }
-    }
-
-    int par(int node) {
-        if(node == parent[node])
-            return node; 
-        return parent[node] = par(parent[node]); 
-    }
-
-    void unionrank(int u, int v) {
-        int ulp_u = par(u); 
-        int ulp_v = par(v); 
-        if(ulp_u == ulp_v) return; 
-        if(rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v; 
-        }
-        else if(rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u; 
-        }
-        else {
-            parent[ulp_v] = ulp_u; 
-            rank[ulp_u]++; 
-        }
-    }
-
-    // void unionBySize(int u, int v) {
-    //     int ulp_u = findUPar(u); 
-    //     int ulp_v = findUPar(v); 
-    //     if(ulp_u == ulp_v) return; 
-    //     if(size[ulp_u] < size[ulp_v]) {
-    //         parent[ulp_u] = ulp_v; 
-    //         size[ulp_v] += size[ulp_u]; 
-    //     }
-    //     else {
-    //         parent[ulp_v] = ulp_u;
-    //         size[ulp_u] += size[ulp_v]; 
-    //     }
-    // }
-    friend class Soluction;
+class DisjointSet{
+  vector<int>rank,size,parent;
+  public:
+    DisjointSet(int n){
+		rank.resize(n+1,0);
+		parent.resize(n+1);
+		size.resize(n+1);
+		for(int i=0;i<=n;i++){
+			size[i]=1;
+			parent[i]=i;
+		}
+	}
+	int findpar(int n){
+		if(n==parent[n])return n;
+		return parent[n]=findpar(parent[n]);
+	}
+	void unionbyrank(int u,int v){
+		int par_u = findpar(u);
+		int par_v = findpar(v);
+		if(par_u==par_v)return;
+        if(rank[par_u]<rank[par_v]){
+			parent[par_u]=par_v;
+		}else if(rank[par_u]>rank[par_v]){
+			parent[par_v]=par_u;
+		}else{
+			rank[par_u]++;
+			parent[par_u]=par_v;
+		}
+	}
+	void unionbysize(int u,int v){
+		int par_u = findpar(u);
+		int par_v = findpar(v);
+		if(par_u==par_v)return;
+        if(size[par_u]<size[par_v]){
+			parent[par_u]=par_v;
+			size[par_v]+=size[par_u];
+		}else{
+			parent[par_u]=par_v;
+			size[par_u]+=size[par_v];
+		}
+	}
 };
+
 class Solution {
-private:
-     void dfs(vector<int>adj[],vector<bool>&vis,int src){
+      void dfs(vector<int>adj[],vector<bool>&vis,int src){
          vis[src]=1;
          for(auto it:adj[src]){
              if(!vis[it]){
@@ -89,29 +52,31 @@ private:
      }
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-        int cnt=0;
         DisjointSet ds(n);
+        int cnt=0;
+        vector<bool>vis(n,0);
         for(int i=0;i<connections.size();i++){
-            if(ds.par(connections[i][0])==ds.par(connections[i][1])){
+            if(ds.findpar(connections[i][0])!=ds.findpar(connections[i][1])){
+                ds.unionbyrank(connections[i][0],connections[i][1]);
+               // vis[connections[i][0]]=1;
+               // vis[connections[i][1]]=1;
+            }else{
                 ++cnt;
-            }else ds.unionrank(connections[i][0],connections[i][1]);
+            }
         }
         vector<int>adj[n];
         for(int i=0;i<connections.size();i++){
             adj[connections[i][0]].push_back(connections[i][1]);
             adj[connections[i][1]].push_back(connections[i][0]);
         }
-        vector<bool>vis(n,0);
-        int curr=0;
+       // vector<bool>vis(n,0);
+        int req=0;
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                ++curr;
+                ++req;
                 dfs(adj,vis,i);
             }
         }
-        cout<<curr<<endl;
-        cout<<cnt<<endl;
-        if((curr-1)<=cnt)return curr-1;
-        return -1;
+        return cnt>=req-1?req-1:-1;
     }
 };
