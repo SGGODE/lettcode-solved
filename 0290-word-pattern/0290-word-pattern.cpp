913bb3fac0b54e499fa3bedc26e1367d4cd8fc27
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-08-24 18:43:52 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (29.45%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string s) {
-        map<char,int>mp;
+        string sx,x;
         int cnt=0;
-        for(auto it:pattern){
-            if(mp.find(it)==mp.end()){
-                mp[it]=cnt+1;
-                ++cnt;
-            }
-        }
-        //for(auto it:mp)cout<<it.first<<" "<<it.second<<endl;
-        //return 1;
-        stringstream ss(s);
-        string word;
-        cnt=0;
+        map<char,int>mp;
         map<string,int>mx;
-        while (ss >> word){
-            if(mx.find(word)==mx.end()){
-                mx[word]=cnt+1;
-                ++cnt;
+        for(auto&it:pattern){
+            if(mp.count(it)){
+                sx.push_back(mp[it]);
+            }else{
+                cnt+=1;
+                mp[it]=cnt;
+                sx.push_back(mp[it]);
             }
         }
-        string pat,res;
-        for(int i=0;i<pattern.size();i++){
-            pat.append(to_string(mp[pattern[i]]));
-           // cout<<mp[pattern[i]]<<endl;
+        string word;
+        stringstream ss(s);
+        cnt=0;
+        while(ss>>word){
+            //cout<<word<<endl;
+            if(mx.count(word)){
+                x.push_back(mx[word]);
+            }else{
+                cnt+=1;
+                mx[word]=cnt;
+                x.push_back(mx[word]);
+            }
         }
-        stringstream sx(s);
-        string wor;
-         while(sx>>wor){
-            res.append(to_string(mx[wor]));
-            // cout<<mx[wor]<<endl;
-        }
-        return pat==res;
+        
+        return sx==x;
     }
 };
