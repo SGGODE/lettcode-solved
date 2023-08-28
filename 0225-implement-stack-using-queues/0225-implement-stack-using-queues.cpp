d3b80b6133d238e12bdc081786755488292d7b34
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-08-28 09:00:59 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (97.83%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -3,23 +3,15 @@ public:
     MyStack() {
         
     }
-    queue<int>pq;
+    deque<int>pq;
     void push(int x) {
-        pq.push(x);
+        pq.push_back(x);
     }
     
     int pop() {
-        vector<int>v;
-        //queue<int>px(pq);
-        while(!pq.empty()){
-            v.push_back(pq.front());
-            pq.pop();
-        }int n=v.size();
-        //pq.clear();
-        for(int i=0;i<n-1;i++){
-          pq.push(v[i]);   
-        }
-        return v[n-1];
+        int x=pq.back();
+        pq.pop_back();
+        return x;
     }
     
     int top() {
