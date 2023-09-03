d3992d861739ded72d9d06c4e165a7dc3db20327
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-09-03 15:23:50 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (24.88%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,12 +1,14 @@
 class Solution {
 private:
-    int sub(vector<vector<int>>&dp,int row,int col,int m,int n){
-        if((row<0||col<0)||(row>=m||col>=n))return 0;
-        if(row==m-1&&col==n-1)return 1;
+    int sub(vector<vector<int>>&dp,int row,int col,int n,int m){
+        if((row<0||col<0)||(row>=n||col>=m))return 0;
+        if(row==n-1&&col==m-1){
+            return 1;
+        }
         if(dp[row][col]!=-1)return dp[row][col];
-        int down = sub(dp,row+1,col,m,n);
-        int left = sub(dp,row,col+1,m,n);
-        return dp[row][col]=left+down;
+        int right = sub(dp,row,col+1,n,m);
+        int down = sub(dp,row+1,col,n,m);
+        return dp[row][col]=right+down;
     }
 public:
     int uniquePaths(int m, int n) {
