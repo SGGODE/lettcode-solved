dc5048a94f77e416a5f7220a05518d2cd6d20edd
sanket gode <88069272+SGGODE@users.noreply.github.com>
2023-09-09 10:02:05 +0530

    Time: 0 ms (100.00%), Space: 6.7 MB (46.24%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,20 +1,21 @@
 class Solution {
 private:
-    int fun(vector<int>&nums,int sum,vector<int>&dp){
-    
-        if(sum==0)return 1;
-        if(dp[sum]!=-1)return dp[sum];
-        int cnt=0;
+    int sub(vector<int>&dp,vector<int>&nums,int index,int target){
+        if(target==0)return 1;
+        if(index==nums.size())return 0;
+       // int notpick = sub(nums,index+1,target);
+        if(dp[target]!=-1)return dp[target];
+        int pick = 0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]<=sum)
-            cnt+=fun(nums,sum-nums[i],dp);
+         if(target>=nums[i]){
+             pick += sub(dp,nums,index,target-nums[i]);
+         }
         }
-        return dp[sum]=cnt;
+        return dp[target]=pick;
     }
 public:
     int combinationSum4(vector<int>& nums, int target) {
-       int n=nums.size();
-       vector<int>dp(target+1,-1);
-       return fun(nums,target,dp); 
+        vector<int>dp(target+1,-1);
+        return sub(dp,nums,0,target);
     }
 };
