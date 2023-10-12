de7fe827f5f1f9d8029161b89c6e154e0aab9db0
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2023-10-12 10:48:00 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (14.97%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.cpp
@@ -9,53 +9,57 @@
  */
 
 class Solution {
+private:
+    int _search(MountainArray &mountainArr,int start,int end,int target){
+        while(start<=end){
+            int mid = (start+end)/2;
+            //cout<<start<<" "<<end<<" "<<mid<<endl;
+            int res = mountainArr.get(mid);
+            //cout<<res<<" "<<target<<endl;
+            if(res==target)return mid;
+            else if(res<target){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return 1e9+7;
+    }
+      int _bsearch(MountainArray &mountainArr,int start,int end,int target){
+        while(start<=end){
+            int mid = (start+end)/2;
+            int res = mountainArr.get(mid);
+            if(res==target)return mid;
+            else if(res<target){
+                end = mid-1;
+            }else{
+                start = mid+1;
+            }
+        }
+        return 1e9+7;
+    }
 public:
     int findInMountainArray(int target, MountainArray &mountainArr) {
         int n = mountainArr.length();
         int s = 0;
         int e = n-1;
-        int mid = (s+e)/2;
-        int index=-1;
+        int pick = -1;
         while(s<=e){
-            int element=mountainArr.get(mid);
-            int x=mountainArr.get(mid+1);
-            if(element>x&&element>mountainArr.get(mid-1)){
-                index=mid;
+            int mid = (s+e)/2;
+            int res =  mountainArr.get(mid);
+            int front =  mountainArr.get(mid+1);
+            if(res>front&&mountainArr.get(mid-1)<res){
+                pick = mid;
                 break;
             }
-            else if(element<x){
+            else if(res<front){
                 s=mid+1;
             }else{
                 e=mid-1;
             }
-            mid=(s+e)/2;
         }
-        s=0;
-        e=index;
-        mid=(s+e)/2;
-        while(s<=e){
-            int element=mountainArr.get(mid);
-            if(element==target)return mid;
-            else if(element<target){
-                s=mid+1;
-            }else{
-                e=mid-1;
-            }
-            mid=(s+e)/2;
-        }
-        s=index;
-        e=n-1;
-        mid=(s+e)/2;
-         while(s<=e){
-            int element=mountainArr.get(mid);
-            if(element==target)return mid;
-            else if(element>target){
-                s=mid+1;
-            }else{
-                e=mid-1;
-            }
-            mid=(s+e)/2;
-        }
-        return -1;
+        cout<<pick<<endl;
+       int res =  min(_search(mountainArr,0,pick,target),_bsearch(mountainArr,pick,n-1,target));
+       return res>=1e9+7?-1:res;
     }
 };
