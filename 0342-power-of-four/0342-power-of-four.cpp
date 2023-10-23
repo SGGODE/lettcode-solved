7282b5fc67a285781d8e99c7637f04c90ed4bc15
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2023-10-23 09:54:54 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (13.31%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfFour(int n) {
+        if(n<1)return 0;
+        if(n==1)return 1;
+        if(n%4!=0)return 0;
+        return isPowerOfFour(n/4);
+    }
+};
