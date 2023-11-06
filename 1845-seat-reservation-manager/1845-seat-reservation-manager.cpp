707e2b6b223c41e28fce7c78b611e1f8f2c3c7d2
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2023-11-06 11:24:12 +0530

    Time: 494 ms (7.32%), Space: 191 MB (8.32%) - LeetHub

--- 1845-seat-reservation-manager/1845-seat-reservation-manager.cpp
+++ 1845-seat-reservation-manager/1845-seat-reservation-manager.cpp
@@ -1,24 +1,15 @@
 class SeatManager {
 public:
-    vector<int>res;
     set<int>s;
     SeatManager(int n) {
-        res.resize(n+1);
         for(int i=1;i<=n;i++)s.insert(i);
     }
     
     int reserve() {
-        auto it=s.begin();
-        int num=*it;
-        // for(int i=1;i<res.size();i++){
-        //     if(!res[i]){
-        //         res[i]=1;
-        //         num=i;
-        //         break;
-        //     }
-        // }
-        s.erase(num);
-        return num;
+        auto it = s.begin();
+        int x = *it;
+        s.erase(x);
+       return x;
     }
     
     void unreserve(int seatNumber) {
