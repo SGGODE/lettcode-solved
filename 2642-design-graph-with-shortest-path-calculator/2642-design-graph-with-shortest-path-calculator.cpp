d1b525cbcc7821ec4b14fd079b7c4f76bc1de4db
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2023-11-11 18:56:33 +0530

    Time: 313 ms (42.02%), Space: 69.8 MB (83.47%) - LeetHub

--- 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.cpp
+++ 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.cpp
@@ -1,36 +1,34 @@
 class Graph {
 public:
     vector<pair<int,int>>adj[101];
-   // vector<int>res(100,INT_MAX);
     Graph(int n, vector<vector<int>>& edges) {
-       // adj.resize(n);
-        for(auto it:edges){
+        for(auto&it:edges){
             adj[it[0]].push_back({it[1],it[2]});
+           // adj[it[1]].push_back({it[0],it[2]});
         }
     }
     
     void addEdge(vector<int> edge) {
         adj[edge[0]].push_back({edge[1],edge[2]});
+       // adj[edge[1]].push_back({edge[0],edge[2]});
     }
     
     int shortestPath(int node1, int node2) {
-        vector<int>res(101,INT_MAX);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        pq.push({0,node1});
-        res[node1]=0;
+        vector<int>dist(101,1e9);
+        dist[node1]=0;
+        queue<int>pq;
+        pq.push(node1);
         while(!pq.empty()){
-            int dist=pq.top().first;
-            int node=pq.top().second;
-            for(auto it:adj[node]){
-                if(res[it.first]>dist+it.second){
-                    res[it.first]=dist+it.second;
-                    pq.push({dist+it.second,it.first});
+            int node = pq.front();
+            pq.pop();
+            for(auto&it:adj[node]){
+                if(dist[node]+it.second<dist[it.first]){
+                    dist[it.first]=dist[node]+it.second;
+                    pq.push(it.first);
                 }
             }
-            pq.pop();
         }
-        if(res[node2]==INT_MAX)return -1;
-        return res[node2];
+        return dist[node2]!=1e9?dist[node2]:-1;
     }
 };
 
