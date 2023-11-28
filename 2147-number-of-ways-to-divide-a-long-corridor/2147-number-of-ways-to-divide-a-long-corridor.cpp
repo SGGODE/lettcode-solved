79c5d780ba231c0a30a4727673a24ed35b3a3159
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2023-11-28 10:24:14 +0530

    Time: 208 ms (31.43%), Space: 40 MB (66.86%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.cpp
@@ -2,37 +2,32 @@ class Solution {
 private:
     int mod = 1e9+7;
     int dp[100001][3];
-    int sub(string&s,vector<int>&res,int index,int cnt){
-        if(index==res.size()){
-            return cnt==2;
-        }
+    int sub(string&s,int index,int cnt){
+        if(index==s.size())return cnt==2;
         if(cnt>2)return 0;
         if(dp[index][cnt]!=-1)return dp[index][cnt];
         int cut = 0;
         if(s[index]=='P'){
-            cut = (cut + sub(s,res,index+1,cnt))%mod;
+            cut = (cut + sub(s,index+1,cnt))%mod;
             if(cnt==2){
-                cut = (cut + sub(s,res,index+1,0))%mod;
+                cut = (cut + sub(s,index+1,0))%mod;
             }
         }else{
             if(cnt==1){
-                cut = (cut + sub(s,res,index+1,0)+sub(s,res,index+1,cnt+1))%mod;
-            }else cut = (cut + sub(s,res,index+1,cnt+1))%mod;
+                cut = (cut + sub(s,index+1,0)+sub(s,index+1,cnt+1))%mod;
+            }else cut = (cut + sub(s,index+1,cnt+1))%mod;
         }
         return dp[index][cnt]=cut;
     }
 public:
     int numberOfWays(string corridor) {
         memset(dp,-1,sizeof(dp));
-        int n = corridor.size();
-        vector<int>res;
         int cnt = 0;
         for(auto&it:corridor){
             if(it=='S')++cnt;
-            res.push_back(cnt);
         }
-        if(res[n-1]==2)return 1;
-        if(res[n-1]%2!=0)return 0;
-        return sub(corridor,res,0,0);
+        if(cnt==2)return 1;
+        if(cnt%2!=0)return 0;
+        return sub(corridor,0,0);
     }
 };
