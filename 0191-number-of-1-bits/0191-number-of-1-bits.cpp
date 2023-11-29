081daa0523f25bc33e3e35339b11c21385d7eed5
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2023-11-29 18:46:07 +0530

    Time: 4 ms (30.61%), Space: 6.4 MB (57.22%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
+        //to count total number of set bits.
         int cnt = 0;
-        while(n!=0){
-            if(n&1!=0)++cnt;
-            n>>=1;
+        while(n!=0){//while number not become zero run the loop
+            if(n&1!=0)++cnt;//simply check weather the current bit at place 1 is set or not it set increment the counter.
+            n>>=1;//shift the bits by place one.
         }
-        return cnt;
+        return cnt;//return final count.
     }
 };
