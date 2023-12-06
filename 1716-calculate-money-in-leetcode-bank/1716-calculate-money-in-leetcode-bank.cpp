5deb7519a6cc3daaab29c87f68df50c9f933374c
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2023-12-06 10:45:40 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (70.53%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int totalMoney(int n) {
-        int start = 1;
-        int last = 1;
-        int total = 1;
-        int step = 1;
+        int curr_money = 1;
+        int lastmonday = 1;
+        int sum = 1;
+        int days = 1;
         --n;
         while(n!=0){
-            ++step;
-            ++start;
-            --n;
-            if(step%8==0){
-                start = last+1;
-                last=start;
-                step=1;
+            ++days;//update day.
+            ++curr_money;//update cost of day.
+            --n;//reduce the no of days remain.
+            if(days%8==0){ //if one cycle is done.
+                curr_money = lastmonday+1; //update the money form last monday.
+                lastmonday = curr_money; //update this monday as last monday for last.
+                days = 1;
             }
-            total+=start;
+            //add the current cost of the day.
+            sum += curr_money;
         }
-        return total;
+        return sum;
     }
 };
