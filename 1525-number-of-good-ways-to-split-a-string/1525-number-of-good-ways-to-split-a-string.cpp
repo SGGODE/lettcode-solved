9a1e18f7289e95562dae737b40c1df06a80d3ebe
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2023-12-07 14:16:58 +0530

    Time: 30 ms (50.34%), Space: 21.8 MB (5.64%) - LeetHub

--- 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
+++ 1525-number-of-good-ways-to-split-a-string/1525-number-of-good-ways-to-split-a-string.cpp
@@ -1,32 +1,45 @@
 class Solution {
 private:
+ //dp to store.
  int dp[100001];
+//helper function who workk on it.
  int sub(int index,vector<int>&prefix,vector<int>&sufix,int n){
-    if(index==n||index==n-1)return 0;
-    if(dp[index]!=-1)return dp[index];
-    int ans = 0;
-    if(index+1<n&&prefix[index]==sufix[index+1])ans += 1 + sub(n,prefix,sufix,n);
-    ans += sub(index+1,prefix,sufix,n);
+    //any way no more cut possible at last position or end.
+       if(index==n||index==n-1)return 0;
+    //if the state already begin calculate.
+        if(dp[index]!=-1)return dp[index];
+    //store current state both possibility if possible cut or don't cut.
+        int ans = 0;
+    //possibility if possible cut it.
+        if(index+1<n&&prefix[index]==sufix[index+1])ans += 1 + sub(n,prefix,sufix,n);
+    //don't cut it.
+        ans += sub(index+1,prefix,sufix,n);
+    //finally store the result.
     return dp[index]=ans;
  }
 public:
     int numSplits(string s) {
+        //intilize dp.
         memset(dp,-1,sizeof(dp));
+        //size of string.
         int n = s.size();
-        vector<int>prefix(n,0);
-        set<char>st;
-        for(int i=0;i<n;i++){
-          st.insert(s[i]);
-          prefix[i]=st.size();
-        }
-        vector<int>sufix(n,0);
-        set<char>stz;
-        for(int i=n-1;i>=0;i--){
-          stz.insert(s[i]);
-          sufix[i]=stz.size();
-        }
-        //cout<<prefix[n-1]<<endl;
-        if(prefix[n-1]==1)return n-1;
+        //prefix no of unique `char` array. 
+            vector<int>prefix(n,0);
+            set<char>st;
+            for(int i=0;i<n;i++){
+              st.insert(s[i]);
+              prefix[i]=st.size();
+            }
+        //sufix no of unique `char` array.
+            vector<int>sufix(n,0);
+            set<char>stz;
+            for(int i=n-1;i>=0;i--){
+              stz.insert(s[i]);
+              sufix[i]=stz.size();
+            }
+        //if there is only one unique charter in string then 
+             if(prefix[n-1]==1)return n-1;
+        //else 
         return sub(0,prefix,sufix,n);
     }
 };
