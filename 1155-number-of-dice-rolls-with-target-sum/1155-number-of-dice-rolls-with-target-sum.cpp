ff2745a805244f68b6b216b91b39d15f6ad6f8e5
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-01-01 17:30:08 +0530

    Time: 22 ms (64.18%), Space: 6.3 MB (97.19%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -5,7 +5,9 @@ private:
     int sub(int n,int k,int target){
         if(n==0&&target==0)return 1;
         if(n==0||target==0)return 0;
-        if(dp[n][target]!=-1)return dp[n][target];
+        if(dp[n][target]!=-1){
+            return dp[n][target];
+        }
         int ans = 0;
         for(int i=1;i<=k;i++){
             if(i<=target){
@@ -17,6 +19,7 @@ private:
 public:
     int numRollsToTarget(int n, int k, int target) {
         memset(dp,-1,sizeof(dp));
+        //cout<<1<<endl;
         return sub(n,k,target);
     }
 };
