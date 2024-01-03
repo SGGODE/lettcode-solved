8c42f03edf850e9a420562bc2329718773132265
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-01-03 08:24:30 +0530

    Time: 84 ms (79.88%), Space: 23.3 MB (96.49%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
-        int cnt=0;
-        int n=bank.size();
-        int m=bank[0].size();
-      //  vector<vector<char>>res(n,vector<char>(m,'.'));
-        map<int,int>mp;
-        for(int i=0;i<n;i++){
-            string s=bank[i];
-            for(int j=0;j<m;j++){
-                if(s[j]=='1'){
-                    //res[i][j]='l';
-                    mp[i]++;
-                }//else mp[i]=0;
+        //return 8;
+        int last = 0;
+        int sum = 0;
+        for(int i=0;i<bank.size();i++){
+            int curr = 0;
+            for(auto&it:bank[i]){
+                if(it=='1')++curr;
             }
+            sum += (last*curr);
+            if(curr!=0)last = curr;
         }
-        for(int i=0;i<n;i++){
-            if(mp.find(i)==mp.end())mp[i]=0;
-        }
-        for(int i=0;i<n;i++){
-           for(int j=i+1;j<n;j++){
-               if(mp[j]!=0){
-                   cnt+=(mp[i]*mp[j]);
-                 //  cout<<i<<endl;
-                   i=j-1;
-                  // cout<<i<<" "<<n<<endl;
-                   break;
-               }
-           }
-             //cout<<i<<" "<<n<<endl;
-        }
-      //  cout<<mp[3]<<endl;
-        return cnt;
+        return sum;
     }
 };
