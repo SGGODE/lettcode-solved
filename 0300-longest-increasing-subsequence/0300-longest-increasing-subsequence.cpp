52d1172f8819174f92991b55e8c9181acc132a38
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-01-05 09:49:13 +0530

    Time: 244 ms (50.94%), Space: 10.8 MB (80.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,26 +1,15 @@
 class Solution {
-private:
-    int sub(vector<vector<int>>&dp,vector<int>&nums,int index,int last){
-        if(index==nums.size()){
-           // if(last>nums[0])return 1;
-            return 0;
-        }
-       if(dp[index][last+1]!=-1)return dp[index][last+1];
-        int notpick=sub(dp,nums,index+1,last);
-        int pick=0;
-        if(last==-1){
-            pick=1+sub(dp,nums,index+1,index);
-        }else{
-            if(nums[index]>nums[last])pick=1+sub(dp,nums,index+1,index);
-        }
-        return dp[index][last+1]=max(pick,notpick);
-    }
 public:
     int lengthOfLIS(vector<int>& nums) {
-        //nums.push_back(1e5);
-        int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return sub(dp,nums,0,-1);
-       // return dp[n-1];
+        int n = nums.size();
+        vector<int>dp(n,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i]=max(dp[i],dp[j]+1);
+                }
+            }
+        }
+        return *max_element(dp.begin(),dp.end());
     }
 };
