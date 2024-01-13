18b9492e116274d09c40007df4b4409b30cd1390
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-01-13 09:00:02 +0530

    Time: 112 ms (19.43%), Space: 17.1 MB (19.43%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -4,12 +4,13 @@ public:
         map<char,int>mp,mx;
         for(auto&it:s)mp[it]++;
         for(auto&it:t)mx[it]++;
-        int cnt=0;
+        if(mp==mx)return 0;
+        int cnt = 0;
         for(auto&it:mp){
-            if(mx.count(it.first)){
-                if(it.second>mx[it.first])cnt+=abs(it.second-mx[it.first]);
-            }else{
-                cnt+=it.second;
+            if(mx.count(it.first)&&it.second>mx[it.first]){
+                cnt += abs(mx[it.first]-it.second);
+            }else if(!mx.count(it.first)){
+                cnt += it.second;
             }
         }
         return cnt;
