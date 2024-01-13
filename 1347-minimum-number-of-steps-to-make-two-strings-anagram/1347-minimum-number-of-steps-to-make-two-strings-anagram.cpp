fbee7570d710dcdecea131665f37835e761c2ea4
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-01-13 09:03:02 +0530

    Time: 50 ms (73.99%), Space: 16.8 MB (82.29%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        map<char,int>mp,mx;
-        for(auto&it:s)mp[it]++;
-        for(auto&it:t)mx[it]++;
-        if(mp==mx)return 0;
-        int cnt = 0;
-        for(auto&it:mp){
-            if(mx.count(it.first)&&it.second>mx[it.first]){
-                cnt += abs(mx[it.first]-it.second);
-            }else if(!mx.count(it.first)){
-                cnt += it.second;
-            }
+        // map<char,int>mp,mx;
+        // for(auto&it:s)mp[it]++;
+        // for(auto&it:t)mx[it]++;
+        // if(mp==mx)return 0;
+        // int cnt = 0;
+        // for(auto&it:mp){
+        //     if(mx.count(it.first)&&it.second>mx[it.first]){
+        //         cnt += abs(mx[it.first]-it.second);
+        //     }else if(!mx.count(it.first)){
+        //         cnt += it.second;
+        //     }
+        // }
+        // return cnt;
+        int cnt[26]={0};
+        for(int i=0;i<s.size();i++){
+            cnt[s[i]-'a']++;
+            cnt[t[i]-'a']--;
         }
-        return cnt;
+        int count = 0;
+        for(int i=0;i<26;i++){
+            count += max(0,cnt[i]);
+        }
+        return count;
     }
 };
