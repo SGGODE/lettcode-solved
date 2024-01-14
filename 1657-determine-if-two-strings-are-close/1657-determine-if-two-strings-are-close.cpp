3804381021f8748ae399d7e5813a190c6140e962
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-01-14 09:32:09 +0530

    Time: 124 ms (44.28%), Space: 21.4 MB (16.73%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,46 +1,21 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        map<char,int>mp1,mp2;
-        for(int i=0;i<word1.size();i++){
-            mp1[word1[i]]++;
+        map<char,int>mp,mx;
+        for(auto&it:word1)mp[it]++;
+        for(auto&it:word2)mx[it]++;
+        vector<int>num1,num2;
+        vector<char>numx,numy;
+        for(auto&it:mp){
+            num1.push_back(it.second);
+            numx.push_back(it.first);
         }
-        for(int i=0;i<word2.size();i++){
-            mp2[word2[i]]++;
+        for(auto&it:mx){
+            num2.push_back(it.second);
+            numy.push_back(it.first);
         }
-        map<int,int>mp;
-        for(auto it:mp1){
-            mp[it.second]++;
-        }
-        for(auto it:mp2){
-            mp[it.second]++;
-        }
-        vector<int>word0,word3;
-        vector<char>word4,word5;
-        for(auto it:mp1){
-           word0.push_back(it.second);
-        }
-         for(auto it:mp2){
-           word3.push_back(it.second);
-        }
-        for(auto it:mp1){
-           word4.push_back(it.first);
-        }
-         for(auto it:mp2){
-           word5.push_back(it.first);
-        }
-        sort(word0.begin(),word0.end());
-        sort(word3.begin(),word3.end());
-        sort(word4.begin(),word4.end());
-        sort(word5.begin(),word5.end());
-        // for(auto it:mp1){
-        //     if(mp2.find(it.first)==mp2.end())return 0;
-        // }
-        // if(mp.size()==1)return 1;
-        // for(auto it:mp){
-        //     if(it.second%2==0)continue;
-        //     else return 0;
-        // }
-        return word0==word3 && word4==word5;
+        sort(num1.begin(),num1.end());sort(numx.begin(),numx.end());
+        sort(num2.begin(),num2.end());sort(numy.begin(),numy.end());
+        return num1==num2&&numx==numy;
     }
 };
