44694b11b2552692869aee5cf28bb16956fd62f8
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-01-19 10:08:45 +0530

    Time: 18 ms (17.72%), Space: 11.1 MB (17.95%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,35 +1,23 @@
 class Solution {
 private:
-    int minpath=INT_MAX,n,m;
-    bool check(int i,int j){
-          if(i<0||j<0||i>=n||j>=m)return 0;
-        return 1;
-    }
-    void call(vector<vector<int>>&matrix,int i,int j,int sum){
-        int minval=INT_MAX;
-       if(check(i-1,j+1))
-         minval=min(minval,(matrix[i][j]+matrix[i-1][j+1]));
-       if(check(i-1,j))
-          minval=min(minval,(matrix[i-1][j]+matrix[i][j]));
-       if(check(i-1,j-1))
-           minval=min(minval,(matrix[i][j]+matrix[i-1][j-1]));
-        matrix[i][j]=minval;
-        //matrix[i][j]=min((matrix[i][j]+matrix[i-1][j+1]),min((matrix[i-1][j]+matrix[i][j]),(matrix[i][j]+matrix[i-1][j-1])));
+    bool check(int row,int col,int&n,int&m){
+        return row>=0&&col>=0&&row<n&&col<m;
     }
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-         n=matrix.size();
-         m=matrix[0].size();
-        for(int i=1;i<n;i++){
-          for(int j=0;j<m;j++){
-              call(matrix,i,j,matrix[i][j]);
-          }  
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<vector<int>>dp(n,vector<int>(m,1e9));
+        for(int i=0;i<m;i++){
+            dp[0][i]=matrix[0][i];
         }
-        for(int i=n-1;i<n;i++){
+        for(int i=1;i<n;i++){
             for(int j=0;j<m;j++){
-                minpath=min(minpath,matrix[i][j]);
+                if(check(i-1,j-1,n,m))dp[i][j]=min(dp[i][j],matrix[i][j]+dp[i-1][j-1]);
+                if(check(i-1,j+1,n,m))dp[i][j]=min(dp[i][j],matrix[i][j]+dp[i-1][j+1]);
+                if(check(i-1,j,n,m))dp[i][j]=min(dp[i][j],matrix[i][j]+dp[i-1][j]);
             }
         }
-        return minpath;
+        return *min_element(dp[n-1].begin(),dp[n-1].end());
     }
 };
