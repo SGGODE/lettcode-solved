25d572751d32ad60f245c799d05ee59ef046d1fd
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-01-30 08:33:07 +0530

    Time: 12 ms (34.50%), Space: 15.3 MB (16.25%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,38 +1,28 @@
 class Solution {
-#define ll long long 
-private:
-    bool check(string ch){
-        if(ch=="+"||ch=="-"||ch=="/"||ch=="*")return 1;
-        return 0;
-    }
 public:
-    int evalRPN(vector<string>& tok) {
-        stack<string>s;
-        for(int i=0;i<tok.size();i++){
-            if(check(tok[i])){
-                ll x=0;
-                x=stoll(s.top());
-                s.pop();
-                ll y=0;y=stoll(s.top());
-                s.pop();
-                if(tok[i]=="+"){
-                 ll z=x+y;
-                  s.push(to_string(z));
-                }
-                if(tok[i]=="-"){
-                 ll z=y-x;
-                  s.push(to_string(z));
-                }
-                if(tok[i]=="/"){
-                 ll z=y/x;
-                  s.push(to_string(z));
-                }
-                if(tok[i]=="*"){
-                 ll z=x*y;
-                  s.push(to_string(z));
-                }
-            }else s.push(tok[i]);
+    int evalRPN(vector<string>& tokens) {
+        stack<int>st;
+        for(int i=0;i<tokens.size();i++){
+            if(tokens[i]=="+"){
+                int x = st.top();st.pop();
+                int y = st.top();st.pop();
+                st.push(x+y);
+            }else if(tokens[i]=="-"){
+                int x = st.top();st.pop();
+                int y = st.top();st.pop();
+                st.push(y-x);
+            }else if(tokens[i]=="*"){
+                int x = st.top();st.pop();
+                int y = st.top();st.pop();
+                st.push(x*y);
+            }else if(tokens[i]=="/"){
+                int x = st.top();st.pop();
+                int y = st.top();st.pop();
+                st.push(floor(y/x));
+            }else{
+                st.push(stoi(tokens[i]));
+            }
         }
-        return stoll(s.top());
+        return st.top();
     }
 };
