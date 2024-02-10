345a86ed6428d5e620e2b899936b925af744ef87
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-02-10 07:06:55 +0530

    Time: 180 ms (13.78%), Space: 13.5 MB (50.02%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+private:
+    bool check(string&s){
+        int n = s.size();
+        for(int i=0;i<n/2;i++){
+            if(s[i]!=s[n-i-1])return 0;
+        }
+        return 1;
+    }
+public:
+    int countSubstrings(string s) {
+        int cnt = 0;
+        for(int i=0;i<s.size();i++){
+            string res;
+            for(int j=i;j<s.size();j++){
+                res.push_back(s[j]);
+                if(check(res))++cnt;
+            }
+        }
+        return cnt;
+    }
+};
