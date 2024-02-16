f23ed76e0a3ed50b0672ccb0d77910c21a74d7bc
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-02-16 09:53:08 +0530

    Time: 202 ms (34.42%), Space: 70.1 MB (24.57%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
     int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
-        unordered_map<int,int>mp;
-        for(auto it:arr)mp[it]++;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        for(auto it:mp){
-            pq.push({it.second,it.first});
+        map<int,int>mp;
+        for(auto&it:arr)mp[it]++;
+        vector<pair<int,int>>res;
+        for(auto&it:mp){
+            res.push_back({it.second,it.first});
         }
-        vector<pair<int,int>>v;
-        while(!pq.empty()){
-            v.push_back({pq.top().first,pq.top().second});
-            pq.pop();
-        }
-        // for(auto it:v)cout<<it.second<<""<<it.first<<" ";
-        // cout<<endl;
-        int j=0;
-        while(k>0){
-           v[j].first-=1;
-           if(v[j].first==0)j++;
-            --k;
-        }
-        //  for(auto it:v)cout<<it.second<<""<<it.first<<" ";
-        // cout<<endl;
-        int cnt=0;
-        for(auto it:v){
-            if(it.first==0)continue;
-            else ++cnt;
-        }
+        sort(res.begin(),res.end());
+        for(int i=0;i<res.size()&&k!=0;i++){
+            if(k>=res[i].first){
+                int x = res[i].first; 
+                res[i].first=0;
+                k-=x;
+            }else{
+                res[i].first=(res[i].first-k);
+                k=0;
+            }
+         }
+        int cnt = 0;
+         for(int i=0;i<res.size();i++){
+             if(res[i].first>0)++cnt;
+         }
         return cnt;
     }
 };
+
+
