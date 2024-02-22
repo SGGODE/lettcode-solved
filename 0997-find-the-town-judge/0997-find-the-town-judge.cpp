67ea6dbeba46d1b731bc3cd06422839bbc577e14
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-02-22 10:00:48 +0530

    Time: 178 ms (17.57%), Space: 81.5 MB (5.08%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int>adj[n+1];
-        for(int i=0;i<trust.size();i++){
-            adj[trust[i][0]].push_back(trust[i][1]);
+        map<int,set<int>>mp;
+        for(auto&it:trust){
+            mp[it[0]].insert(it[1]);
         }
         for(int i=1;i<=n;i++){
-            if(adj[i].empty()){
-                bool flag=1;vector<int>::iterator it;
-               for(int j=1;j<=n;j++){
-                   if(j==i)continue;
-                   else{
-                   it=find(adj[j].begin(),adj[j].end(),i);
-                   if(it==adj[j].end()){
-                       flag=0;
+            if(!mp.count(i)){
+                bool flag = 1;
+                for(int j=1;j<=n;j++){
+                    if(i!=j&&!mp[j].count(i)){
+                        flag=0;
+                        break;
                     }
-                  }
-               }
-                if(flag)
-                return i;
+                }
+                if(flag)return i;
             }
         }
         return -1;
