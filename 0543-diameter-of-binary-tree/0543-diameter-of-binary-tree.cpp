bdf3aebb9fd70d14f142aa2ade0b2b94adf5ee80
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-02-27 10:57:37 +0530

    Time: 17 ms (13.26%), Space: 18.8 MB (90.86%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,30 +11,20 @@
  */
 class Solution {
 private:
-    // void postorder(TreeNode* root,vector<int>&v){
-    //     if(root==NULL)return;
-    //     postorder(root->left,v);
-    //     postorder(root->right,v);
-    //     v.push_back(root->val);
-    // }
-    int height(TreeNode* root){
+    int ans = 0;
+    void sub(TreeNode* root){
+        if(root==NULL)return;
+        ans = max(ans,(hr(root->left)+hr(root->right)));
+        sub(root->left);
+        sub(root->right);
+    }
+    int hr(TreeNode* root){
         if(root==NULL)return 0;
-        int l=height(root->left);
-        int r=height(root->right);
-        return max(l,r)+1;
+        return max(1+hr(root->left),1+hr(root->right));
     }
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        if(root==NULL)return 0;
-        // vector<int>v;
-        // postorder(root,v);
-        // int x=v.size();
-        // return x-1;
-        // return 1;
-        int l1=diameterOfBinaryTree(root->left);
-        int r1=diameterOfBinaryTree(root->right);
-        int l=height(root->left);
-        int r=height(root->right);
-        return max(l1,max(r1,(l+r)));
+        sub(root);
+        return ans;
     }
 };
