4f4fe5e14a3fe2b9da2855d2960f63a5b5771433
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-02-28 08:39:21 +0530

    Time: 3 ms (98.29%), Space: 20.3 MB (63.53%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -12,45 +12,29 @@
 class Solution {
 public:
     int findBottomLeftValue(TreeNode* root) {
-        if(root==NULL)return 0;
-        if(root->left==NULL&&root->right==NULL)return root->val;
-        vector<pair<int,pair<char,int>>>v; 
         queue<TreeNode*>pq;
-        TreeNode* z=new TreeNode(INT_MAX);
-        pq.push(z);
         pq.push(root);
-        pq.push(z);
-        int cnt=0;
+        TreeNode *temp = new TreeNode(1000);
+        pq.push(temp);
+        vector<int>value;
         while(!pq.empty()){
-            TreeNode* temp=pq.front();
-            pq.pop();bool flag=0;
-            if(pq.front()==z){
-                flag=1;
+            TreeNode* node = pq.front();
+            pq.pop();
+            value.push_back(node->val);
+            if(node->left){
+                 pq.push(node->left);
+            }
+            if(node->right){
+                pq.push(node->right);
+            }
+            if(pq.front()==temp){
                 pq.pop();
-            }
-            if(temp->left){
-                v.push_back({cnt+1,{'L',temp->left->val}});
-                pq.push(temp->left);
-            }
-            if(temp->right){
-                v.push_back({cnt+1,{'R',temp->right->val}});
-                pq.push(temp->right);
-            }
-            if(flag){
-                 ++cnt;
-                 pq.push(z);
-            }
-           
-        }
-        int maxval=INT_MIN;
-        int max_node=2147483648;
-        for(auto it:v){
-            if(it.first>maxval){
-                maxval=it.first;
-                max_node=it.second.second;
+                if(!pq.empty()){
+                    value.clear();
+                    pq.push(temp);
+                }
             }
         }
-        if(max_node==2147483648)return root->val;
-        return max_node;
+        return value[0];
     }
 };
