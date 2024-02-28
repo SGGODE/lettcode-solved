775f3363edcd60fb3d2cf40287c16bc9c7c0c172
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-02-28 14:53:37 +0530

    Time: 1186 ms (13.77%), Space: 292.8 MB (11.33%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        int n=nums.size();
-        set<vector<int>>res;
+        set<vector<int>>st;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n-2;i++){
-            int s=i+1;
-            int e=n-1;
-            while(s<e){
-                if(nums[i]+nums[s]+nums[e]==0){
-                    res.insert({nums[i],nums[s],nums[e]});
-                    s++;
-                    e--;
+        map<int,int>mp;
+        for(auto&it:nums)mp[it]++;
+        for(int i=0;i<nums.size();i++){
+            int s = i+1,e=nums.size()-1;
+                while(s<e){
+                 //   int mid = (s+e)/2;
+                    if((nums[i]+nums[s]+nums[e])==0){
+                        st.insert({nums[i],nums[s],nums[e]});
+                        s++;
+                        e--;
+                    }
+                    if((nums[i]+nums[s]+nums[e])>0){
+                        e -= 1;
+                    }else if((nums[i]+nums[s]+nums[e])<0){
+                        s += 1;
+                    }
                 }
-                if(nums[i]+nums[s]+nums[e]>0){
-                    e--;
-                }
-                 if(nums[i]+nums[s]+nums[e]<0){
-                    s++;
-                }
-            }
         }
         vector<vector<int>>result;
-        for(auto&it:res){
-            vector<int>temp(it.begin(),it.end());
-            result.push_back(temp);
+        for(auto&it:st){
+            result.push_back(it);
         }
         return result;
     }
