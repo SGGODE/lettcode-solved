03540d8df3789d1828c154e989f7a3f73db93cac
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-03-06 02:08:25 +0530

    Time: 38 ms (5.10%), Space: 15.8 MB (5.10%) - LeetHub

--- 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
+++ 1750-minimum-length-of-string-after-deleting-similar-ends/1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        deque<char>pq;
      if(s.size()==1)return 1;
-    for(auto&it:s)pq.push_back(it);
+     deque<char>pq;
+     for(auto&it:s)pq.push_back(it);
+     char x = pq.front();
+     char y = pq.back();
 
-    //gquiz.push_back(10);
+     while(!pq.empty()&&x==y) {
 
-    char x = pq.front();
+      while(!pq.empty()&&pq.front()==x )pq.pop_front();
 
-    char y = pq.back();
+      while(!pq.empty()&&pq.back()==y)pq .pop_back();
 
-    while(!pq.empty()&&x==y) {
-
-    while(!pq.empty()&&pq.front()==x )pq.pop_front();
-
-    while(!pq.empty()&&pq.back()==y)pq .pop_back();
-
-    if(pq.size()>1){
-
-    x = pq.front(); y = pq.back();
-
-    }else break;
-
-    }
-
-    return pq.size();
+      if(pq.size()>1){x = pq.front();y = pq.back();}
+      else break;
+     }
+     return pq.size();
     }
 };
