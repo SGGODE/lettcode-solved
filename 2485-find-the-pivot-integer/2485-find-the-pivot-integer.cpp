92477a7cb56208ab4fcc032de6d854decdc867df
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-03-13 10:14:16 +0530

    Time: 4 ms (35.26%), Space: 7.2 MB (73.82%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        vector<int>res(n+1,0);
         int sum = 0;
+        int total = (n*(n+1))/2;
         for(int i=1;i<=n;i++){
+            if(sum+i==(total-sum))return i;
             sum+=i;
-            res[i]=sum;
         }
-        for(int i=1;i<=n;i++)if(res[i]==(res[n]-res[i-1]))return i;
-        
         return -1;
     }
 };
