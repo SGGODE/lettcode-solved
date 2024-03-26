23bced963dadf3b701855fe16e66a699d8067f6a
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-03-26 08:28:52 +0530

    Time: 127 ms (5.12%), Space: 60.7 MB (11.54%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        map<int,int>mp;
-        for(auto &it:nums)mp[it]++;
-        int m=*max_element(nums.begin(),nums.end());
-        for(int i=1;m>0&&i<=m;i++){
-            if(mp.count(i)==0)return i;
+        int n = nums.size();
+        set<int>mp;
+        for(auto&it:nums)mp.insert(it);
+        for(int i=1;i<=n;i++){
+            if(!mp.count(i))return i;
         }
-        if(m>0)return m+1;
-        return 1;
+        return n+1;
     }
 };
