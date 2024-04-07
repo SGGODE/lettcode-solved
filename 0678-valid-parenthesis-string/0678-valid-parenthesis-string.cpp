d07afd1d09a338dc7523db4d73faeb1d87555b03
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-04-07 10:39:53 +0530

    Time: 2 ms (47.10%), Space: 7.6 MB (27.25%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,79 +1,23 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-       int n=s.size();
-        int cnt=0;
-      //  int ecnt=0;
-       //  stack<char>st;
-       // for(int i=0;i<n;i++){
-       //     if(s[i]=='*')++cnt;
-       //     if(s[i]==')'){
-       //       //  stack<char>st(s);
-       //         while(!st.empty()&&st.top()!='('){
-       //             st.pop();
-       //         }
-       //         if(st.empty()&&cnt==0)return 0;
-       //         if(st.empty()&&cnt!=0)cnt-=1;
-       //         else st.pop();
-       //     }
-       //  //   if(s[i]!='(')
-       //      st.push(s[i]); 
-       // }
-       //  return st.empty();
-       //  if(n==1){
-       //      if(s[0]=='*')return 1;
-       //      else return 0;
-       //  }
-       //  //stack<char>st;
-       //  int lcnt=0;
-       //  for(int i=0;i<n;i++){
-       //    if(s[i]=='('){
-       //        cnt++;
-       //        lcnt--;
-       //    }
-       //    if(s[i]==')'){
-       //        cnt--;
-       //        lcnt--;
-       //    }
-       //    else{
-       //        lcnt--;
-       //       cnt++; 
-       //    } 
-       //    if(cnt<0)return 0;
-       //  if(lcnt<0)lcnt=0;
-       //  }
-       // // while(st)
-       // // return st.empty();
-       //  return lcnt==0;
-       //  
-         stack<int> open;
-        stack<int> star;
-
-        for(int i = 0; i < s.size(); i++){
-            if(s[i]=='('){
-                open.push(i);
-            }
-            else if(s[i]=='*'){
-                star.push(i);;
-            }
-            else {
-                if(s[i]==')'){
-                    if(!open.empty()){
-                        open.pop();
-                    }
-                    else if(!star.empty()){
-                        star.pop();
-                    }
-                    else return false;
-                }
-            }
-
+         int n = s.size();
+         int j = 0;
+         int cnt = 0;
+         stack<int>st,star;
+          for(;j<n;j++){
+              if(s[j]==')'){
+                 if(!st.empty())st.pop();
+                 else if(!star.empty())star.pop();
+                 else return 0; 
+             }
+             else if(s[j]=='(')st.push(j);
+             else star.push(j);
         }
-        while(!open.empty() && !star.empty() && open.top() < star.top()){
-            open.pop();
+        while(!st.empty()&&!star.empty()&&st.top()<star.top()){
+            st.pop();
             star.pop();
         }
-
-        return open.empty();
+        return st.empty();
     }
 };
