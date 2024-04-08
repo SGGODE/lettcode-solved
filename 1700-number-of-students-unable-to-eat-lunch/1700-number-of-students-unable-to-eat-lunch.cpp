46d4d4ef7d770fe173256b7b311e92e60a4a9303
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-04-08 11:03:27 +0530

    Time: 0 ms (100.00%), Space: 11.2 MB (6.64%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>&sand) {
-        map<int,int>mp,mx;
+        map<int,int>mp;
         queue<int>pq;
         for(auto&it:students){
             mp[it]++;
             pq.push(it);
         }
-        for(auto&it:sand)mx[it]++;
         int j = 0;
         while(!pq.empty()){
             int node = pq.front();
             if(!mp.count(sand[j]))break;
             pq.pop();
             if(node==sand[j]){
-                --mx[sand[j]];
-                if(mx[sand[j]]==0)mx.erase(sand[j]);
-                j++;
+                 j++;
                 --mp[node];
                 if(mp[node]==0)mp.erase(node);
             }else{
