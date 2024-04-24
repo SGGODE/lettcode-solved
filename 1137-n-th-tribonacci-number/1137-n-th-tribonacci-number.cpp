d715bda43782e03d00f99d08a299cdd2610b865f
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-04-24 08:23:28 +0530

    Time: 3 ms (22.77%), Space: 7.7 MB (5.38%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
+    map<int,int>dp;
     int tribonacci(int n) {
         if(n==0)return 0;
         if(n==1||n==2)return 1;
-        vector<int>dp(n+1,0);
-        dp[0]=0,dp[1]=1,dp[2]=1;
-        for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
-        }
-        return dp[n];
+        if(dp.count(n))return dp[n];
+        return dp[n]=tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
     }
 };
