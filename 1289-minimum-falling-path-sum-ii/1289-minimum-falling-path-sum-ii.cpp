63b50e19cd0c21b9c507559b06c970363a30e8fb
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-04-26 08:40:16 +0530

    Time: 398 ms (13.84%), Space: 18.1 MB (67.84%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -5,14 +5,9 @@ private:
        if(row==grid.size())return 0;
        if(dp[row][col+1]!=-1)return dp[row][col+1];
        int ans = 1e9;
-       if(col==-1){
-           for(int i=0;i<grid[0].size();i++){
-               ans = min(ans,grid[row][i]+sub(grid,row+1,i));
-           }
-       }else{
-          for(int i=0;i<grid[0].size();i++){
-               if(i!=col)ans = min(ans,grid[row][i]+sub(grid,row+1,i));
-          } 
+       for(int i=0;i<grid[0].size();i++){
+          if(col==-1)ans = min(ans,grid[row][i]+sub(grid,row+1,i));
+          else if(i!=col)ans = min(ans,grid[row][i]+sub(grid,row+1,i));
        }
        return dp[row][col+1]=ans;
    }
