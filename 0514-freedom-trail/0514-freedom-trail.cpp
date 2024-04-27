366acf3e73d046fbe386ab0d2ad23aea4616dbce
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-04-27 08:59:26 +0530

    Time: 27 ms (49.76%), Space: 13.7 MB (69.10%) - LeetHub

---
+++ 0514-freedom-trail/0514-freedom-trail.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private:
+    vector<vector<int>>dp;
+    int sub(string&ring,string&key,int ind,int kind,int n){
+        if(kind==key.size())return 0;
+        if(dp[ind][kind]!=-1)return dp[ind][kind];
+        int ans = 1e9;
+        for(int i=0;i<n;i++){
+           if(ring[(ind+i)%n]==key[kind]){
+               ans = min(ans,(i+1)+sub(ring,key,(ind+i)%n,kind+1,n));
+           } 
+        }
+        int cnt = 1;
+        for(int i=n-1;i>=0;i--){
+           if(ring[(ind+i)%n]==key[kind]){
+               ans = min(ans,(cnt+1)+sub(ring,key,(ind+i)%n,kind+1,n));
+           } 
+           ++cnt;
+        }
+        return dp[ind][kind]=ans;
+    }
+public:
+    int findRotateSteps(string ring, string key) {
+        int n = ring.size();
+        int m = key.size();
+        dp.resize(n,vector<int>(m,-1));
+        return sub(ring,key,0,0,n);
+    }
+};
