450e0d584b6fab2f714c7ef36a62adfb001d85fd
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-05-18 16:49:19 +0530

    Time: 305 ms (5.09%), Space: 11.2 MB (8.77%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,36 +1,38 @@
 class Solution {
+private:
+bool compare(map<char,int>&mp,map<char,int>&mx){
+    for(auto&it:mp){
+       if(!(mx.count(it.first)&&it.second<=mx[it.first])){
+         return 0;
+       }
+    }
+  return 1;
+}
 public:
-    string minWindow(string s, string t) {
-        int fs[256]={0};
-        int fp[256]={0};
-        for(int i=0;i<t.size();i++)
-            fp[t[i]]++;
-        
-        int c=0;
-        int start=0;
-        int window_size;
-        int start_idx=-1;
-        int min_so_far=INT_MAX;
-        for(int i=0;i<s.length();i++){
-            char ch=s[i];
-            fs[ch]++;
-            if(fs[ch]!=0 && fs[ch] <= fp[ch])
-                c++;
-            if(c==t.length()){
-                while(fp[s[start]]==0 || fs[s[start]] > fp[s[start]]){
-                    fs[s[start]]--;
-                    start++;
+    string minWindow(string str, string arr) {
+        int n = str.size();
+          map<char,int>mp;
+          for(auto&it:arr)mp[it]++;
+          int j = 0;
+          int len = INT_MAX;
+          int targeti=-1,targetj=-1;
+          map<char,int>mx;
+          for(int i=0;i<n;i++){
+             mx[str[i]]++;
+             while(compare(mp,mx)&&j<=i){
+                if(len>(i-j)+1){
+                   len = (i-j)+1;
+                   targeti=i;
+                   targetj=j;
                 }
-                window_size=i-start+1;
-                if(window_size < min_so_far){
-                  min_so_far=window_size;
-                    start_idx=start;
-                }
-            }
-        }
-        if(start_idx==-1)
-            return "";
-        
-        return s.substr(start_idx,min_so_far);
+                --mx[str[j]];
+                if(mx[str[j]]==0)mx.erase(str[j]);
+                j++;
+             }
+          }
+          string x;
+          if(targeti!=-1&&targetj!=-1)for(int i=targetj;i<=targeti;i++)x.push_back(str[i]);
+          return x.empty()?"":x;
+
     }
 };
