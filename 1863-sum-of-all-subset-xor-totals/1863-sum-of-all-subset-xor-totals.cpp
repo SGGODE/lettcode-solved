33d3921ada6039336f47755c0cb498fdc2db4017
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-05-20 08:23:21 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (40.27%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.cpp
@@ -1,17 +1,17 @@
 class Solution {
 private:
-    int dp[13][100];
+    //int dp[13][100];
     int sub(vector<int>&nums,int index,int Xor){
         if(index==nums.size())return Xor;
-        if(dp[index][Xor]!=-1)return dp[index][Xor];
+      //  if(dp[index][Xor]!=-1)return dp[index][Xor];
         int ans = 0;
         ans += sub(nums,index+1,Xor);
         ans += sub(nums,index+1,Xor^nums[index]);
-        return dp[index][Xor]=ans;
+        return ans;
     }
 public:
     int subsetXORSum(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
+      //  memset(dp,-1,sizeof(dp));
         return sub(nums,0,0);
     }
 };
