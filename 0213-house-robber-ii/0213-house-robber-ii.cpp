63cf12047396fe3bbf99c13b617faa58043a8832
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-06-03 13:44:47 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (84.38%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,29 +1,19 @@
 class Solution {
+private:
+    int dp[101][101];
+    int sub(vector<int>&nums,int index,int n){
+        if(index>(n-1))return 0;
+        if(dp[index][n]!=-1)return dp[index][n];
+        int ans = 0;
+        ans = max(ans,nums[index]+sub(nums,index+2,n));
+        ans = max(ans,sub(nums,index+1,n));
+        return dp[index][n]=ans;
+    }
 public:
     int rob(vector<int>& nums) {
-       int n=nums.size();
+        memset(dp,-1,sizeof(dp));
+        int n = nums.size();
         if(n==1)return nums[0];
-        if(n==2)return max(nums[0],nums[1]);
-       vector<int>dp1(n,-1);
-       vector<int>dp2(n,-1);
-        dp1[0]=nums[0];
-        dp1[1]=nums[1];
-        for(int i=2;i<n-1;i++){
-            int ans=INT_MIN;
-            for(int j=i-2;j>=0;j--){
-                ans=max(ans,nums[i]+dp1[j]);
-            }
-            dp1[i]=ans;
-        }
-        dp2[1]=nums[1];
-        dp2[2]=nums[2];
-        for(int i=3;i<n;i++){
-            int ans=INT_MIN;
-            for(int j=i-2;j>=0;j--){
-                ans=max(ans,nums[i]+dp2[j]);
-            }
-            dp2[i]=ans;
-        }
-        return max(*max_element(dp1.begin(),dp1.end()),*max_element(dp2.begin(),dp2.end()));
+        return max(sub(nums,0,n-1),sub(nums,1,n));
     }
 };
