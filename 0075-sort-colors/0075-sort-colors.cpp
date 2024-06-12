6433a642c60a7d2a365ca2125a100430108a8b58
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-06-12 22:35:02 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (34.14%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int start = 0 , mid = 0 , end = nums.size()-1;
+        while(mid <= end){
+            if(nums[mid]==0){
+                swap(nums[mid],nums[start]);
+                ++mid;++start;
+            }
+            else if(nums[mid]==1){
+                ++mid;
+            }else{
+                swap(nums[mid],nums[end]);
+                --end;
+            }
+        }
+    }
+};
