fb2104d07680af44c671b0f452d701c4d2f42e50
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-06-22 08:22:13 +0530

    Time: 167 ms (13.62%), Space: 93.9 MB (15.50%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-       // int oddcnt=0;
-       int n=nums.size();
-       //  for(int i=0;i<n;i++){
-       //      if(nums[i]%2!=0)oddcnt++;
-       //  }
-       //  int cnt=0;
-       //  int oddcnt1=oddcnt;
-       //  for(int i=0;i<n;i++){
-       //      if(oddcnt>=k)cnt++;
-       //      if(nums[i]%2!=0)--oddcnt1;
-       //  }
-       //  for(int i=n-1;i>=0;i--){
-       //      if(oddcnt>=k)cnt++;
-       //      if(nums[i]%2!=0)--oddcnt;
-       //  }
-       //  return cnt;
-        for(int i=0;i<n;i++){
-            if(nums[i]%2==0)nums[i]=0;
-            else nums[i]=1;
+        unordered_map<int , int>odd_count;
+        int total = 0;
+        int count = 0;
+        odd_count[count]++;
+        for(auto& it : nums){
+            if(it%2)++count;
+            if(odd_count.count(count-k))total += odd_count[count-k];
+            odd_count[count]++;
         }
-        int sum=0;
-        map<int,int>mp;
-        mp[sum]=1;
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            sum+=nums[i];
-           // if(sum==k)++cnt;
-            if(mp.find(sum-k)!=mp.end())cnt+=mp[sum-k];
-            mp[sum]++;
-        }
-        return cnt;
+        return total;
     }
 };
