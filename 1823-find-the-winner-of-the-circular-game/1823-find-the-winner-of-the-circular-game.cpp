c702369dce972c1f75758ddca520ed186ccbcf6a
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-07-08 07:02:47 +0530

    Time: 27 ms (15.66%), Space: 25.8 MB (5.16%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,67 +1,18 @@
 class Solution {
-private:
-    // void fun(int n,int k,vector<bool>&vis,int index,int mark){
-    //     if(mark==n-1)return;
-    //     if(vis[index]==0){
-    //         vis[index]=1;
-    //         if((index+k)>n){
-    //             int temp=n-index;
-    //             int add=k-temp;
-    //             fun(n,k,vis,add,mark+1);
-    //         }
-    //         else fun(n,k,vis,index+k,mark+1);
-    //     }else{
-    //         if((index+1)>n)fun(n,k,vis,1,mark);
-    //         else fun(n,k,vis,index+1,mark);
-    //     } 
-    // }
-    int next(int index,vector<bool>&vis){
-        int n=vis.size();
-        for(int i=index+1;i<n;i++){
-            if(vis[i]==0)return i;
-        }
-        for(int i=1;i<n;i++){
-            if(vis[i]==0)return i;
-        }
-        return -1;
-    }
 public:
     int findTheWinner(int n, int k) {
-        if(n==1&&k==1)return 1;
-        vector<bool>vis(n+1,0);
-        int mark=1;
-        vis[k]=1;
-        int j=k+1;
-        if(k==n)j=1;
-        while(mark!=n-1){
-            int cnt=0;
-            while(cnt!=k){
-             bool flag=0;
-              for(int p=j;p<=n;p++){
-                  if(vis[p]==0){
-                      ++cnt;
-                  }
-                  if(cnt==k){
-                      j=p;
-                      flag=1;
-                      break;
-                  }
-              }
-             if(flag){
-                vis[j]=1;
-                ++mark;
-                j=next(j,vis);
-                 break;
-             }else{
-                 j=1;
-             }
-            }
+        queue<int>pq;
+        for(int i = 1 ; i <= n ; i++){
+            pq.push(i);
         }
-        for(int i=1;i<=n;i++){
-            if(vis[i]==0){
-                return i;
+        while(pq.size()>1){
+            for(int i = 1 ; i < k ; i++){
+                int x = pq.front();
+                pq.pop();
+                pq.push(x);
             }
+            pq.pop();
         }
-        return 0;
+        return pq.front();
     }
 };
