5bc89f0b48960b9fc34f0f80d98181b68d047a23
Sanket Gode <88069272+SGGODE@users.noreply.github.com>
2024-12-26 15:58:27 +0530

    Time: 263 ms (38.72%), Space: 62.1 MB (10.54%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,11 +1,13 @@
 class Solution {
 private:
+    map<pair<int,int>,int>dp;
     int solve(vector<int>&nums , int target , int index){
         if(index == nums.size())return target == 0;
+        if(dp.count({target , index}))return dp[{target , index}];
         int ans = 0;
         ans += solve(nums , target - nums[index] , index + 1);
         ans += solve(nums , target + nums[index] , index + 1);
-        return ans;
+        return dp[{target , index}] = ans;
     }
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
